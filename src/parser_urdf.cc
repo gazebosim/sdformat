3e23e95ec1227b9f85aebc6d3fcba6b24b00d169
Brian Gerkey <gerkey@osrfoundation.org>
2013-07-09 18:06:55 -0700

    Only insert robot origin pose if it was successfully parsed from the URDF

--- src/parser_urdf.cc
+++ src/parser_urdf.cc
@@ -40,6 +40,7 @@ bool g_enforceLimits;
 std::string g_collisionExt = "_geom";
 std::string g_visualExt = "_visual";
 urdf::Pose g_initialRobotPose;
+bool g_initialRobotPoseValid = false;
 
 /// \brief parser xml string into urdf::Vector3
 /// \param[in] _key XML key where vector3 value might be
@@ -1001,19 +1002,23 @@ void ParseRobotOrigin(TiXmlDocument &_urdfXml)
         std::string(originXml->Attribute("xyz")));
     urdf::Vector3 rpy = ParseVector3(std::string(originXml->Attribute("rpy")));
     g_initialRobotPose.rotation.setFromRPY(rpy.x, rpy.y, rpy.z);
+    g_initialRobotPoseValid = true;
   }
 }
 
 /////////////////////////////////////////////////
 void InsertRobotOrigin(TiXmlElement *_elem)
 {
-  /* set transform */
-  double pose[6];
-  pose[0] = g_initialRobotPose.position.x;
-  pose[1] = g_initialRobotPose.position.y;
-  pose[2] = g_initialRobotPose.position.z;
-  g_initialRobotPose.rotation.getRPY(pose[3], pose[4], pose[5]);
-  AddKeyValue(_elem, "pose", Values2str(6, pose));
+  if (g_initialRobotPoseValid)
+  {
+    /* set transform */
+    double pose[6];
+    pose[0] = g_initialRobotPose.position.x;
+    pose[1] = g_initialRobotPose.position.y;
+    pose[2] = g_initialRobotPose.position.z;
+    g_initialRobotPose.rotation.getRPY(pose[3], pose[4], pose[5]);
+    AddKeyValue(_elem, "pose", Values2str(6, pose));
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
