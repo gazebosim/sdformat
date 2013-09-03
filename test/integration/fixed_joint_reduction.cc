25248534ec6718676cea31467c1e667a93a4cbf9
Steve Peters <scpeters@osrfoundation.org>
2013-09-03 14:51:37 -0700

    Reduce indentation

--- test/integration/fixed_joint_reduction.cc
+++ test/integration/fixed_joint_reduction.cc
@@ -25,96 +25,96 @@ const std::string SDF_TEST_FILE = std::string(PROJECT_SOURCE_PATH)
 
 TEST(SDFParser, FixedJointReductionEquivalenceTest)
 {
-    sdf::SDFPtr robot(new sdf::SDF());
-    sdf::init(robot);
-    ASSERT_TRUE(sdf::readFile(SDF_TEST_FILE, robot));
+  sdf::SDFPtr robot(new sdf::SDF());
+  sdf::init(robot);
+  ASSERT_TRUE(sdf::readFile(SDF_TEST_FILE, robot));
 
-    sdf::ElementPtr model = robot->root->GetElement("model");
-    for (sdf::ElementPtr link = model->GetElement("link"); link;
-         link = link->GetNextElement("link"))
-    {
-      std::string linkName = link->Get<std::string>("name");
-      sdf::ElementPtr inertial = link->GetElement("inertial");
-      double linkMass = inertial->Get<double>("mass");
-      sdf::Pose linkPose = inertial->Get<sdf::Pose>("pose");
-      sdf::ElementPtr inertia = inertial->GetElement("inertia");
-      double ixx = inertia->Get<double>("ixx");
-      double iyy = inertia->Get<double>("iyy");
-      double izz = inertia->Get<double>("izz");
-      double ixy = inertia->Get<double>("ixy");
-      double ixz = inertia->Get<double>("ixz");
-      double iyz = inertia->Get<double>("iyz");
+  sdf::ElementPtr model = robot->root->GetElement("model");
+  for (sdf::ElementPtr link = model->GetElement("link"); link;
+       link = link->GetNextElement("link"))
+  {
+    std::string linkName = link->Get<std::string>("name");
+    sdf::ElementPtr inertial = link->GetElement("inertial");
+    double linkMass = inertial->Get<double>("mass");
+    sdf::Pose linkPose = inertial->Get<sdf::Pose>("pose");
+    sdf::ElementPtr inertia = inertial->GetElement("inertia");
+    double ixx = inertia->Get<double>("ixx");
+    double iyy = inertia->Get<double>("iyy");
+    double izz = inertia->Get<double>("izz");
+    double ixy = inertia->Get<double>("ixy");
+    double ixz = inertia->Get<double>("ixz");
+    double iyz = inertia->Get<double>("iyz");
 
-      std::cout << "name [" << linkName
-                << "] m[" << linkMass
-                << "] p[" << linkPose
-                << "] ixx[" << ixx
-                << "] iyy[" << iyy
-                << "] izz[" << izz
-                << "] ixy[" << ixy
-                << "] ixz[" << ixz
-                << "] iyz[" << iyz
-                << "]\n";
+    std::cout << "name [" << linkName
+              << "] m[" << linkMass
+              << "] p[" << linkPose
+              << "] ixx[" << ixx
+              << "] iyy[" << iyy
+              << "] izz[" << izz
+              << "] ixy[" << ixy
+              << "] ixz[" << ixz
+              << "] iyz[" << iyz
+              << "]\n";
 
 
-       if (linkName == "link0")
-       {
-         EXPECT_EQ(linkMass, 1000);
-         EXPECT_EQ(linkPose, sdf::Pose(0, 0, -0.5, 0, -0, 0));
-         EXPECT_EQ(ixx, 1);
-         EXPECT_EQ(iyy, 1);
-         EXPECT_EQ(izz, 1);
-         EXPECT_EQ(ixy, 0);
-         EXPECT_EQ(ixz, 0);
-         EXPECT_EQ(iyz, 0);
-       }
-       else if (linkName == "link1")
-       {
-         EXPECT_EQ(linkMass, 100);
-         EXPECT_EQ(linkPose,
-                   sdf::Pose(0, -1.5, 0, -2.14159, 0.141593, 0.858407));
-         EXPECT_EQ(ixx, 2);
-         EXPECT_EQ(iyy, 3);
-         EXPECT_EQ(izz, 4);
-         EXPECT_EQ(ixy, 0);
-         EXPECT_EQ(ixz, 0);
-         EXPECT_EQ(iyz, 0);
-       }
-       else if (linkName == "link2")
-       {
-         EXPECT_EQ(linkMass, 200);
-         EXPECT_EQ(linkPose,
-                   sdf::Pose(0.2, 0.4, 1, -1.14159, -0.141593, 1.5708));
-         EXPECT_EQ(ixx, 5);
-         EXPECT_EQ(iyy, 6);
-         EXPECT_EQ(izz, 7);
-         EXPECT_EQ(ixy, 0);
-         EXPECT_EQ(ixz, 0);
-         EXPECT_EQ(iyz, 0);
-       }
-       else if (linkName == "link3")
-       {
-         EXPECT_EQ(linkMass, 400);
-         EXPECT_EQ(linkPose,
-                   sdf::Pose(0.1, 0.2, 0.3, -1.14159, 0.141593, 0.858407));
-         EXPECT_EQ(ixx, 8);
-         EXPECT_EQ(iyy, 9);
-         EXPECT_EQ(izz, 10);
-         EXPECT_EQ(ixy, 0);
-         EXPECT_EQ(ixz, 0);
-         EXPECT_EQ(iyz, 0);
-       }
-       else if (linkName == "link1a")
-       {
-         EXPECT_EQ(linkMass, 700);
-         EXPECT_EQ(linkPose,
-             sdf::Pose(1.6, -2.72857, -0.342857, -2.14159, 0.141593, 0.858407));
-         EXPECT_EQ(ixx, 576.477);
-         EXPECT_EQ(iyy, 527.681);
-         EXPECT_EQ(izz, 420.127);
-         EXPECT_EQ(ixy, -65.6808);
-         EXPECT_EQ(ixz, 134.562);
-         EXPECT_EQ(iyz, 264.781);
-       }
-    }
+     if (linkName == "link0")
+     {
+       EXPECT_EQ(linkMass, 1000);
+       EXPECT_EQ(linkPose, sdf::Pose(0, 0, -0.5, 0, -0, 0));
+       EXPECT_EQ(ixx, 1);
+       EXPECT_EQ(iyy, 1);
+       EXPECT_EQ(izz, 1);
+       EXPECT_EQ(ixy, 0);
+       EXPECT_EQ(ixz, 0);
+       EXPECT_EQ(iyz, 0);
+     }
+     else if (linkName == "link1")
+     {
+       EXPECT_EQ(linkMass, 100);
+       EXPECT_EQ(linkPose,
+                 sdf::Pose(0, -1.5, 0, -2.14159, 0.141593, 0.858407));
+       EXPECT_EQ(ixx, 2);
+       EXPECT_EQ(iyy, 3);
+       EXPECT_EQ(izz, 4);
+       EXPECT_EQ(ixy, 0);
+       EXPECT_EQ(ixz, 0);
+       EXPECT_EQ(iyz, 0);
+     }
+     else if (linkName == "link2")
+     {
+       EXPECT_EQ(linkMass, 200);
+       EXPECT_EQ(linkPose,
+                 sdf::Pose(0.2, 0.4, 1, -1.14159, -0.141593, 1.5708));
+       EXPECT_EQ(ixx, 5);
+       EXPECT_EQ(iyy, 6);
+       EXPECT_EQ(izz, 7);
+       EXPECT_EQ(ixy, 0);
+       EXPECT_EQ(ixz, 0);
+       EXPECT_EQ(iyz, 0);
+     }
+     else if (linkName == "link3")
+     {
+       EXPECT_EQ(linkMass, 400);
+       EXPECT_EQ(linkPose,
+                 sdf::Pose(0.1, 0.2, 0.3, -1.14159, 0.141593, 0.858407));
+       EXPECT_EQ(ixx, 8);
+       EXPECT_EQ(iyy, 9);
+       EXPECT_EQ(izz, 10);
+       EXPECT_EQ(ixy, 0);
+       EXPECT_EQ(ixz, 0);
+       EXPECT_EQ(iyz, 0);
+     }
+     else if (linkName == "link1a")
+     {
+       EXPECT_EQ(linkMass, 700);
+       EXPECT_EQ(linkPose,
+           sdf::Pose(1.6, -2.72857, -0.342857, -2.14159, 0.141593, 0.858407));
+       EXPECT_EQ(ixx, 576.477);
+       EXPECT_EQ(iyy, 527.681);
+       EXPECT_EQ(izz, 420.127);
+       EXPECT_EQ(ixy, -65.6808);
+       EXPECT_EQ(ixz, 134.562);
+       EXPECT_EQ(iyz, 264.781);
+     }
+  }
 }
