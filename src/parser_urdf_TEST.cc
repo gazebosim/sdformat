fb75f73d991695b888c89a8813387c7a458089b3
Steve Peters <scpeters@osrfoundation.org>
2014-03-18 10:09:21 -0700

    Fix parser_urdf_TEST

--- src/parser_urdf_TEST.cc
+++ src/parser_urdf_TEST.cc
@@ -40,7 +40,7 @@ class URDFParser : public ::testing::Test
       std::string get_minimal_sdf_txt()
       {
           std::ostringstream stream;
-          stream << "<sdf version='" << SDF_VERSION << "'>"
+          stream << "<sdf version='" << "1.4" << "'>"
                  << "<model name='test_robot' />"
                  << "</sdf>";
           return stream.str();
