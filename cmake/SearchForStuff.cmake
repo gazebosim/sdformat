d68a25920b748186c9b572018f8d17f615d7c309
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-05-20 19:03:56 +0200

    Revert changes to leave only tinyxml

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -4,54 +4,8 @@ include (FindPkgConfig)
 
 ########################################
 # Find Boost, if not specified manually
-
-if (WIN32)
-    # The following verifyies that BOOST_ROOT is set properly.
-    if(NOT BOOST_ROOT AND NOT $ENV{BOOST_ROOT} STREQUAL "")
-        FILE( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
-        if( NOT EXISTS ${BOOST_ROOT} ) 
-            MESSAGE( STATUS  ${BOOST_ROOT} " does not exist. Checking if BOOST_ROOT was a quoted string.." )
-            STRING( REPLACE "\"" "" BOOST_ROOT ${BOOST_ROOT} ) 
-            if( EXISTS ${BOOST_ROOT} ) 
-                MESSAGE( STATUS "After removing the quotes " ${BOOST_ROOT} " was now found by CMake" )
-            endif( EXISTS ${BOOST_ROOT})
-        endif( NOT EXISTS ${BOOST_ROOT} )
-
-	# Save the BOOST_ROOT in the cache
-	if( NOT EXISTS ${BOOST_ROOT} ) 
-	    MESSAGE( WARNING ${BOOST_ROOT} " does not exist." )
-	else(NOT EXISTS ${BOOST_ROOT})
-	    SET (BOOST_ROOT ${BOOST_ROOT} CACHE STRING "Set the value of BOOST_ROOT to point to the root folder of your boost install." FORCE)
-	endif( NOT EXISTS ${BOOST_ROOT} )
-    endif(NOT BOOST_ROOT AND NOT $ENV{BOOST_ROOT} STREQUAL "")
-
-    if(BOOST_ROOT)
-        MESSAGE(WARNING "Please set the BOOST_ROOT environment variable.")
-    endif()
-
-    set(Boost_DEBUG ON)
-    set(Boost_USE_STATIC_LIBS        ON)
-    set(Boost_USE_MULTITHREADED      OFF)
-    set(Boost_USE_STATIC_RUNTIME     ON)
-endif()
-
 include(FindBoost)
 find_package(Boost ${MIN_BOOST_VERSION} REQUIRED system filesystem program_options regex iostreams)
-
-#FIND_PACKAGE(Boost 1.47.0 COMPONENTS ${BOOST_COMPONENTS_NEEDED})
-if(Boost_FOUND)
-    MESSAGE( STATUS "Setting up boost." )
-    include_directories(${Boost_INCLUDE_DIRS})
-    if(Boost_DEBUG) 
-	MESSAGE( STATUS "BOOST Libraries " ${Boost_LIBRARIES} )
-	FOREACH(BOOST_COMPONENT ${BOOST_COMPONENTS_NEEDED})
-	    STRING( TOUPPER ${BOOST_COMPONENT} BOOST_COMPONENT_UPCASE )
-	    MESSAGE( STATUS "Boost " ${BOOST_COMPONENT} ": " ${Boost_${BOOST_COMPONENT_UPCASE}_LIBRARY} )
-	    MESSAGE( STATUS "Boost " ${BOOST_COMPONENT} " Debug: " ${Boost_${BOOST_COMPONENT_UPCASE}_LIBRARY_DEBUG} )
-	    MESSAGE( STATUS "Boost " ${BOOST_COMPONENT} " Release: " ${Boost_${BOOST_COMPONENT_UPCASE}_LIBRARY_RELEASE} )
-	ENDFOREACH(BOOST_COMPONENT)
-    endif(Boost_DEBUG)
-endif(Boost_FOUND)
 
 if (NOT Boost_FOUND)
   set (BUILD_SDF OFF CACHE INTERNAL "Build SDF" FORCE)
