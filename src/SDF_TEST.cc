b0b6f3a0183afa6575238883607927ebb01be9b4
Nate Koenig <natekoenig@gmail.com>
2015-04-01 08:02:17 -0700

    Fix build

--- src/SDF_TEST.cc
+++ src/SDF_TEST.cc
@@ -507,7 +507,7 @@ TEST_F(RmlUpdate, GetAny)
     try
     {
       EXPECT_EQ(boost::any_cast<sdf::Color>(anyValue),
-          sdf::Color(0.1f, 0.1f, 0.1f, 1f));
+          sdf::Color(0.1f, 0.1f, 0.1f, 1.0f));
     }
     catch(boost::bad_any_cast &/*_e*/)
     {
