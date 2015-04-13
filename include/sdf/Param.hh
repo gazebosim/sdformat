60fe272c74dd3bc7000186906622c80371bb89f2
Nathan Koenig <natekoenig@gmail.com>
2015-04-13 17:24:26 +0000

    Fix template inline

--- include/sdf/Param.hh
+++ include/sdf/Param.hh
@@ -112,7 +112,8 @@ namespace sdf
     /// \brief Set the update function. The updateFunc will be used to
     /// set the parameter's value when Param::Update is called.
     /// \param[in] _updateFunc Function pointer to an update function.
-    public: template<typename T> void SetUpdateFunc(T _updateFunc);
+    public: template<typename T> 
+            void SetUpdateFunc(T _updateFunc);
 
     /// \brief Set the parameter's value using the updateFunc.
     /// \sa Param::SetUpdateFunc
