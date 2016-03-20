cef3ab601fc4129c8f85d53e236e6f7821b7c69d
Silvio Traversaro <silvio.traversaro@iit.it>
2016-03-20 16:33:40 +0100

    Do not export symbols of functions in private headers

--- include/sdf/parser_private.hh
+++ include/sdf/parser_private.hh
@@ -28,30 +28,23 @@
 namespace sdf
 {
   // \brief Initialize the SDF interface using a TinyXML document
-  SDFORMAT_VISIBLE
   bool initDoc(TiXmlDocument *_xmlDoc, SDFPtr _sdf);
 
   // \brief Initialize and SDF Element using a TinyXML document
-  SDFORMAT_VISIBLE
   bool initDoc(TiXmlDocument *_xmlDoc, ElementPtr _sdf);
 
   // \brief For internal use only. Do not use this function.
-  SDFORMAT_VISIBLE
   bool initXml(TiXmlElement *_xml, ElementPtr _sdf);
 
   /// \brief Populate the SDF values from a TinyXML document
-  SDFORMAT_VISIBLE
   bool readDoc(TiXmlDocument *_xmlDoc, SDFPtr _sdf, const std::string &_source);
 
-  SDFORMAT_VISIBLE
   bool readDoc(TiXmlDocument *_xmlDoc, ElementPtr _sdf,
                const std::string &_source);
 
   // \brief For internal use only. Do not use this function.
-  SDFORMAT_VISIBLE
   bool readXml(TiXmlElement *_xml, ElementPtr _sdf);
 
-  SDFORMAT_VISIBLE
   void copyChildren(ElementPtr _sdf, TiXmlElement *_xml);
 }
 #endif
