36594738f1ae2de7508ff3932afe56dfd73b5e16
caguero <devnull@localhost>
2016-04-01 10:35:12 -0700

    Use whitespace trick in cmake vars.

--- cmake/DefaultCFlags.cmake
+++ cmake/DefaultCFlags.cmake
@@ -54,15 +54,15 @@ if (UNIX)
 endif()
 
 # Compiler-specific C++11 activation.
-if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
+if ("${CMAKE_CXX_COMPILER_ID} " MATCHES "GNU ")
     execute_process(
         COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
     if (NOT (GCC_VERSION VERSION_GREATER 4.7))
         message(FATAL_ERROR "${PROJECT_NAME} requires g++ 4.8 or greater.")
     endif ()
-elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
+elseif ("${CMAKE_CXX_COMPILER_ID} " MATCHES "Clang ")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+elseif ("${CMAKE_CXX_COMPILER_ID} " STREQUAL "MSVC ")
     if (MSVC_VERSION LESS 1800)
         message(FATAL_ERROR "${PROJECT_NAME} requires VS 2013 or greater.")
     endif()
