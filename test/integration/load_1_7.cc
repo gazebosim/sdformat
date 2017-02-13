90215c80b703eb9d2c8d50961ba54f01f8a9ebf5
Shane Loretz <sloretz@osrfoundation.org>
2017-02-13 15:11:33 -0800

    Added <names_to_ignore> element to ray sensor

--- test/integration/load_1_7.cc
+++ test/integration/load_1_7.cc
@@ -23,5 +23,47 @@ TEST(SDF1_7, Load1_7World)
   sdf::ElementPtr worldElem = sdf->Root()->GetElement("world");
   ASSERT_NE(nullptr, worldElem);
   EXPECT_EQ(worldElem->Get<std::string>("name"), "1_7_world_loaded");
+}
 
+/////////////////////////////////////////////////
+/// Test that  <name> in <names_to_ignore> are retrievable
+TEST(SDF1_7, RayNamesToIgnore)
+{
+  std::string xmlString = R"(
+<?xml version="1.0" ?>
+<sdf version="1.7">
+  <model name="testmodel">
+    <link name="testlink">
+      <sensor name="world_ray" type="ray">
+        <ray>
+          <names_to_ignore>
+            <name>asdf</name>
+          </names_to_ignore>
+        </ray>
+      </sensor>
+    </link>
+  </model>
+</sdf>)";
+
+  sdf::SDFPtr sdf(new sdf::SDF());
+  sdf->SetFromString(xmlString);
+  ASSERT_NE(nullptr, sdf->Root());
+
+  sdf::ElementPtr modelElem = sdf->Root()->GetElement("model");
+  ASSERT_NE(nullptr, modelElem);
+
+  sdf::ElementPtr linkElem = modelElem->GetElement("link");
+  ASSERT_NE(nullptr, linkElem);
+
+  sdf::ElementPtr sensorElem = linkElem->GetElement("sensor");
+  ASSERT_NE(nullptr, sensorElem);
+
+  sdf::ElementPtr rayElem = sensorElem->GetElement("ray");
+  ASSERT_NE(nullptr, rayElem);
+
+  sdf::ElementPtr namesElem =rayElem->GetElement("names_to_ignore");
+  ASSERT_NE(nullptr, namesElem);
+
+  sdf::ElementPtr nameElem = namesElem->GetElement("name");
+  EXPECT_EQ("asdf", nameElem->GetValue()->GetAsString());
 }
