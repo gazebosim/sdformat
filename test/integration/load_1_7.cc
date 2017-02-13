aca4de21d2a85a4dabf66ffa6976f8bc7865291a
Shane Loretz <sloretz@osrfoundation.org>
2017-02-13 08:38:10 -0800

    SDF can now load version 1.7 files
    Exposes bug with Converter, it always converts to latest even when toVersion is not latest
    ignore vim files

---
+++ test/integration/load_1_7.cc
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "sdf/sdf.hh"
+
+#include "test_config.h"
+
+/////////////////////////////////////////////////
+/// Test conversion using the parser sdf string converter interface.
+TEST(ConverterIntegration, ParserStringConverter)
+{
+  // The gravity and magnetic_field in 1.5 format
+  std::string xmlString = R"(
+<?xml version="1.0" ?>
+<sdf version="1.7">
+  <world name="1_7_world_loaded">
+  </world>
+</sdf>)";
+
+  sdf::SDFPtr sdf(new sdf::SDF());
+  sdf->SetFromString(xmlString);
+
+  ASSERT_TRUE(sdf->Root() != NULL);
+  EXPECT_EQ(sdf->Root()->GetName(), "sdf");
+
+  sdf::ElementPtr worldElem = sdf->Root()->GetElement("world");
+  ASSERT_TRUE(worldElem != NULL);
+  EXPECT_EQ(worldElem->Get<std::string>("name"), "1_7_world_loaded");
+
+}
