a8868a4447514405896953925d2a1dce258f1173
Chris Lalancette <clalancette@osrfoundation.org>
2017-02-14 17:59:43 -0500

    Update the copyright headers on a bunch of files.

--- include/sdf/Console.hh
+++ include/sdf/Console.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Nate Koenig
+ * Copyright 2011-2017 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-*/
+ */
 
 #ifndef _SDF_CONSOLE_HH_
 #define _SDF_CONSOLE_HH_
