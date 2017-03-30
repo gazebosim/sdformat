772d531e274d213ab50f7fef5402e75420f36969
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-03-31 00:35:38 +0200

    Fix problem with memory tool and python3

--- tools/get_mem_info.py
+++ tools/get_mem_info.py
@@ -9,4 +9,4 @@ p = psutil.Process(pid);
 # note that different versions of psutil have different API
 # some versions have memory_info(), others have get_memory_info()
 memory_info = (p.memory_info() if hasattr(p, 'memory_info') else p.get_memory_info());
-print memory_info[0]
+print (memory_info[0])
