0c85552761fc06f830d37ed171d07bcf3524e6c1
Chris Lalancette <clalancette@osrfoundation.org>
2017-04-04 16:00:39 -0400

    Implement Param::GetDefault using stringstream instead of lexical_cast.

--- include/sdf/Param.hh
+++ include/sdf/Param.hh
@@ -307,9 +307,12 @@ namespace sdf
   template<typename T>
   bool Param::GetDefault(T &_value) const
   {
+    std::stringstream ss;
+
     try
     {
-      _value = boost::lexical_cast<T>(this->dataPtr->defaultValue);
+      ss << this->dataPtr->defaultValue;
+      ss >> _value;
     }
     catch(...)
     {
@@ -320,6 +323,7 @@ namespace sdf
              << "type[" << typeid(T).name() << "]\n";
       return false;
     }
+
     return true;
   }
 
