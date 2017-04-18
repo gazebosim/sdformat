16fd72504b7ffb250ebf412807275a20d89c8e7d
Chris Lalancette <clalancette@osrfoundation.org>
2017-04-18 15:30:18 -0400

    Switch the license on Filesystem.cc to Boost.
    Since we took most of the code from there, we should
    use that license.

--- src/Filesystem.cc
+++ src/Filesystem.cc
@@ -1,18 +1,15 @@
 /*
- * Copyright 2017 Open Source Robotics Foundation
+ * Copyright 2002-2009, 2014 Beman Dawes
+ * Copyright 2001 Dietmar Kuehl
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Distributed under the Boost Software License, Version 1.0.
+ * See http://www.boost.org/LICENSE_1_0.txt
+ */
+
+/*
+ * Most of this code was borrowed from Boost in
+ * libs/filesystem/src/operations.cpp and
+ * libs/filesystem/include/boost/filesystem/operations.hpp.
  */
 
 #include <codecvt>
