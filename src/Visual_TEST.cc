680ebdfb4ac68b295ad7ecc64b97b0a4eef72a73
Steven Peters <scpeters@osrfoundation.org>
2019-01-04 18:09:43 +0000

    Visual_TEST.cc: fix windows warnings

--- src/Visual_TEST.cc
+++ src/Visual_TEST.cc
@@ -75,12 +75,13 @@ TEST(DOMVisual, SetMaterial)
   sdf::Material material;
   material.SetAmbient(ignition::math::Color(0, 0.5, 0));
   material.SetDiffuse(ignition::math::Color(1, 0, 0));
-  material.SetSpecular(ignition::math::Color(0, 0.1, 0.9));
+  material.SetSpecular(ignition::math::Color(0f, 0.1f, 0.9f));
 
   visual.SetMaterial(material);
 
   ASSERT_NE(nullptr, visual.Material());
   EXPECT_EQ(ignition::math::Color(0, 0.5, 0), visual.Material()->Ambient());
   EXPECT_EQ(ignition::math::Color(1, 0, 0), visual.Material()->Diffuse());
-  EXPECT_EQ(ignition::math::Color(0, 0.1, 0.9), visual.Material()->Specular());
+  EXPECT_EQ(ignition::math::Color(0f, 0.1f, 0.9f),
+            visual.Material()->Specular());
 }
