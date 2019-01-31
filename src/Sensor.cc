eb0f6de1f804476be29993f838d16522115ae273
Nate Koenig <natekoenig@gmail.com>
2019-01-31 09:06:07 -0800

    rename enum RAY to LIDAR

--- src/Sensor.cc
+++ src/Sensor.cc
@@ -121,9 +121,9 @@ Errors Sensor::Load(ElementPtr _sdf)
   {
     this->dataPtr->type = SensorType::GPS;
   }
-  else if (type == "gpu_ray")
+  else if (type == "gpu_ray" || type == "gpu_lidar")
   {
-    this->dataPtr->type = SensorType::GPU_RAY;
+    this->dataPtr->type = SensorType::GPU_LIDAR;
   }
   else if (type == "imu")
   {
@@ -141,9 +141,9 @@ Errors Sensor::Load(ElementPtr _sdf)
   {
     this->dataPtr->type = SensorType::MULTICAMERA;
   }
-  else if (type == "ray")
+  else if (type == "ray" || type == "lidar")
   {
-    this->dataPtr->type = SensorType::RAY;
+    this->dataPtr->type = SensorType::LIDAR;
   }
   else if (type == "rfid")
   {
