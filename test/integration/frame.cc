9da959240b9fb57c83c0de4899d79e5a67583fc2
Steve Peters <scpeters@openrobotics.org>
2019-10-04 16:55:39 -0700

    add tests to ensure that //pose with empty value is interpreted as identity pose

--- test/integration/frame.cc
+++ test/integration/frame.cc
@@ -593,6 +593,8 @@ TEST(DOMFrame, LoadModelFramesRelativeTo)
   sdf::Root root;
   EXPECT_TRUE(root.Load(testFile).empty());
 
+  using Pose = ignition::math::Pose3d;
+
   // Get the first model
   const sdf::Model *model = root.ModelByIndex(0);
   ASSERT_NE(nullptr, model);
@@ -600,7 +602,7 @@ TEST(DOMFrame, LoadModelFramesRelativeTo)
   EXPECT_EQ(1u, model->LinkCount());
   EXPECT_NE(nullptr, model->LinkByIndex(0));
   EXPECT_EQ(nullptr, model->LinkByIndex(1));
-  EXPECT_EQ(ignition::math::Pose3d(0, 0, 0, 0, 0, 0), model->Pose());
+  EXPECT_EQ(Pose(0, 0, 0, 0, 0, 0), model->Pose());
   EXPECT_EQ("", model->PoseRelativeTo());
 
   EXPECT_TRUE(model->LinkNameExists("L"));
@@ -610,26 +612,75 @@ TEST(DOMFrame, LoadModelFramesRelativeTo)
   EXPECT_EQ(0u, model->JointCount());
   EXPECT_EQ(nullptr, model->JointByIndex(0));
 
-  EXPECT_EQ(4u, model->FrameCount());
+  EXPECT_EQ(5u, model->FrameCount());
   EXPECT_NE(nullptr, model->FrameByIndex(0));
   EXPECT_NE(nullptr, model->FrameByIndex(1));
   EXPECT_NE(nullptr, model->FrameByIndex(2));
   EXPECT_NE(nullptr, model->FrameByIndex(3));
-  EXPECT_EQ(nullptr, model->FrameByIndex(4));
+  EXPECT_NE(nullptr, model->FrameByIndex(4));
+  EXPECT_EQ(nullptr, model->FrameByIndex(5));
   ASSERT_TRUE(model->FrameNameExists("F0"));
   ASSERT_TRUE(model->FrameNameExists("F1"));
   ASSERT_TRUE(model->FrameNameExists("F2"));
   ASSERT_TRUE(model->FrameNameExists("F3"));
+  ASSERT_TRUE(model->FrameNameExists("F4"));
 
   EXPECT_TRUE(model->FrameByName("F0")->AttachedTo().empty());
   EXPECT_EQ("L", model->FrameByName("F1")->AttachedTo());
   EXPECT_EQ("L", model->FrameByName("F2")->AttachedTo());
   EXPECT_TRUE(model->FrameByName("F3")->AttachedTo().empty());
+  EXPECT_TRUE(model->FrameByName("F4")->AttachedTo().empty());
 
   EXPECT_TRUE(model->FrameByName("F0")->PoseRelativeTo().empty());
   EXPECT_TRUE(model->FrameByName("F1")->PoseRelativeTo().empty());
   EXPECT_TRUE(model->FrameByName("F2")->PoseRelativeTo().empty());
   EXPECT_EQ("L", model->FrameByName("F3")->PoseRelativeTo());
+  EXPECT_EQ("F3", model->FrameByName("F4")->PoseRelativeTo());
+
+  EXPECT_EQ(Pose(0, 1, 0, 0, 0, 0), model->FrameByName("F0")->Pose());
+  EXPECT_EQ(Pose(0, 0, 1, 0, 0, 0), model->FrameByName("F1")->Pose());
+  EXPECT_EQ(Pose(0, 0, 2, 0, 0, 0), model->FrameByName("F2")->Pose());
+  EXPECT_EQ(Pose(0, 0, 3, 0, 0, 0), model->FrameByName("F3")->Pose());
+  EXPECT_EQ(Pose::Zero, model->FrameByName("F4")->Pose());
+
+  Pose pose;
+
+  // Test resolvePose for each frame with its relative_to value.
+  // Numbers should match the raw pose value in the model file.
+  EXPECT_TRUE(model->FrameByName("F0")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(0, 1, 0, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F0")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(0, 1, 0, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F1")->ResolvePose("L", pose).empty());
+  EXPECT_EQ(Pose(0, 0, 1, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F2")->ResolvePose("L", pose).empty());
+  EXPECT_EQ(Pose(0, 0, 2, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F3")->ResolvePose("L", pose).empty());
+  EXPECT_EQ(Pose(0, 0, 3, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F4")->ResolvePose("F3", pose).empty());
+  EXPECT_EQ(Pose::Zero, pose);
+
+  //// Test ResolvePose for each Frame relative to the model frame.
+  EXPECT_TRUE(model->FrameByName("F0")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(0, 1, 0, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F0")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(0, 1, 0, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F1")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(1, 0, 1, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F1")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(1, 0, 1, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F2")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(1, 0, 2, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F2")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(1, 0, 2, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F3")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(1, 0, 3, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F3")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(1, 0, 3, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F4")->ResolvePose("__model__", pose).empty());
+  EXPECT_EQ(Pose(1, 0, 3, 0, 0, 0), pose);
+  EXPECT_TRUE(model->FrameByName("F4")->ResolvePose(pose).empty());
+  EXPECT_EQ(Pose(1, 0, 3, 0, 0, 0), pose);
 }
 
 /////////////////////////////////////////////////
