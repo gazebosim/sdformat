54607c1926eae851aff1da8b7aa0f12cd202b1eb
Addisu Z. Taddese <addisu@openrobotics.org>
2019-11-25 17:57:14 -0600

    Suggestions for PR #601: Add CanonicalLink

--- src/Model_TEST.cc
+++ src/Model_TEST.cc
@@ -60,8 +60,10 @@ TEST(DOMModel, Construction)
   EXPECT_FALSE(model.JointNameExists("default"));
 
   EXPECT_TRUE(model.CanonicalLinkName().empty());
+  EXPECT_EQ(nullptr, model.CanonicalLink());
   model.SetCanonicalLinkName("link");
   EXPECT_EQ("link", model.CanonicalLinkName());
+  EXPECT_EQ(nullptr, model.CanonicalLink());
 
   EXPECT_EQ(ignition::math::Pose3d::Zero, model.RawPose());
   EXPECT_TRUE(model.PoseRelativeTo().empty());
