0bb559c156b6cb3dfd2b21a69be716e3789e8f28
Luca Della Vedova <luca@openrobotics.org>
2020-02-07 16:17:40 +0800

    First draft of surface DOM, for now only stores collide bitmasks

--- src/Collision.cc
+++ src/Collision.cc
@@ -19,6 +19,7 @@
 #include <ignition/math/Pose3.hh>
 #include "sdf/Collision.hh"
 #include "sdf/Geometry.hh"
+#include "sdf/Surface.hh"
 #include "sdf/Error.hh"
 #include "sdf/Types.hh"
 #include "Utils.hh"
@@ -36,8 +37,11 @@ class sdf::CollisionPrivate
   /// \brief Frame of the pose.
   public: std::string poseRelativeTo = "";
 
-  /// \brief The collisions's a geometry.
+  /// \brief The collision's geometry.
   public: Geometry geom;
+
+  /// \brief The collision's surface parameters.
+  public: Surface surf;
 
   /// \brief The SDF element pointer used during load.
   public: sdf::ElementPtr sdf;
@@ -126,6 +130,9 @@ Errors Collision::Load(ElementPtr _sdf)
   Errors geomErr = this->dataPtr->geom.Load(_sdf->GetElement("geometry"));
   errors.insert(errors.end(), geomErr.begin(), geomErr.end());
 
+  // Load the surface parameters
+  this->dataPtr->surf.Load(_sdf->GetElement("surface"));
+
   return errors;
 }
 
@@ -151,6 +158,18 @@ const Geometry *Collision::Geom() const
 void Collision::SetGeom(const Geometry &_geom)
 {
   this->dataPtr->geom = _geom;
+}
+
+/////////////////////////////////////////////////
+const Surface *Collision::Surf() const
+{
+  return &this->dataPtr->surf;
+}
+
+/////////////////////////////////////////////////
+void Collision::SetSurf(const Surface &_surf)
+{
+  this->dataPtr->surf = _surf;
 }
 
 /////////////////////////////////////////////////
