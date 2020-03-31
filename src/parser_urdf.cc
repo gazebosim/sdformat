3fa45fc80597cda5d59761bf8f5693eaf36f3aeb
Addisu Z. Taddese <addisu@openrobotics.org>
2020-03-31 18:48:29 -0500

    parentLinkName -> relativeToAttr

--- src/parser_urdf.cc
+++ src/parser_urdf.cc
@@ -2978,12 +2978,12 @@ void CreateJoint(TiXmlElement *_root,
     AddTransform(
         joint, CopyPose(_link->parent_joint->parent_to_joint_origin_transform));
     auto pose = joint->FirstChildElement("pose");
-    std::string parentLinkName = _link->getParent()->name;
-    if ("world" == parentLinkName)
+    std::string relativeToAttr = _link->getParent()->name;
+    if ("world" == relativeToAttr )
     {
-      parentLinkName = "__model__";
+      relativeToAttr = "__model__";
     }
-    pose->SetAttribute("relative_to", parentLinkName);
+    pose->SetAttribute("relative_to", relativeToAttr);
 
     AddKeyValue(joint, "child", _link->name);
     AddKeyValue(joint, "parent", _link->getParent()->name);
