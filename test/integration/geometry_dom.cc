0a36eb89f38709ddd126f3a8e4f9e6421a13ee8f
Louise Poubel <louise@openrobotics.org>
2020-10-12 21:14:31 -0700

    Add Heightmap class
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- test/integration/geometry_dom.cc
+++ test/integration/geometry_dom.cc
@@ -19,11 +19,12 @@
 #include <gtest/gtest.h>
 
 #include "sdf/Box.hh"
-#include "sdf/Cylinder.hh"
 #include "sdf/Collision.hh"
+#include "sdf/Cylinder.hh"
 #include "sdf/Element.hh"
 #include "sdf/Filesystem.hh"
 #include "sdf/Geometry.hh"
+#include "sdf/Heightmap.hh"
 #include "sdf/Link.hh"
 #include "sdf/Mesh.hh"
 #include "sdf/Model.hh"
@@ -31,8 +32,8 @@
 #include "sdf/Root.hh"
 #include "sdf/Sphere.hh"
 #include "sdf/Types.hh"
-#include "sdf/World.hh"
 #include "sdf/Visual.hh"
+#include "sdf/World.hh"
 #include "test_config.h"
 
 //////////////////////////////////////////////////
@@ -136,7 +137,7 @@ TEST(DOMGeometry, Shapes)
   EXPECT_EQ(sdf::GeometryType::MESH, meshCol->Geom()->Type());
   const sdf::Mesh *meshColGeom = meshCol->Geom()->MeshShape();
   ASSERT_NE(nullptr, meshColGeom);
-  EXPECT_EQ("https://ignitionfuel.org/an_org/models/a_model/mesh/mesh.dae",
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/mesh/mesh.dae",
       meshColGeom->Uri());
   EXPECT_TRUE(ignition::math::Vector3d(0.1, 0.2, 0.3) ==
       meshColGeom->Scale());
@@ -150,10 +151,67 @@ TEST(DOMGeometry, Shapes)
   EXPECT_EQ(sdf::GeometryType::MESH, meshVis->Geom()->Type());
   const sdf::Mesh *meshVisGeom = meshVis->Geom()->MeshShape();
   ASSERT_NE(nullptr, meshVisGeom);
-  EXPECT_EQ("https://ignitionfuel.org/an_org/models/a_model/mesh/mesh.dae",
-      meshVisGeom->Uri());
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/mesh"
+      "/mesh.dae", meshVisGeom->Uri());
   EXPECT_TRUE(ignition::math::Vector3d(1.2, 2.3, 3.4) ==
       meshVisGeom->Scale());
   EXPECT_EQ("another_submesh", meshVisGeom->Submesh());
   EXPECT_FALSE(meshVisGeom->CenterSubmesh());
+
+  // Test heightmap collision
+  auto heightmapCol = link->CollisionByName("heightmap_col");
+  ASSERT_NE(nullptr, heightmapCol);
+  ASSERT_NE(nullptr, heightmapCol->Geom());
+  EXPECT_EQ(sdf::GeometryType::HEIGHTMAP, heightmapCol->Geom()->Type());
+  auto heightmapColGeom = heightmapCol->Geom()->HeightmapShape();
+  ASSERT_NE(nullptr, heightmapColGeom);
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/heightmap.png", heightmapColGeom->Uri());
+  EXPECT_EQ(ignition::math::Vector3d(500, 500, 100), heightmapColGeom->Size());
+  EXPECT_EQ(ignition::math::Vector3d(1, 2, 3), heightmapColGeom->Position());
+  EXPECT_EQ(0u, heightmapColGeom->TextureCount());
+  EXPECT_EQ(0u, heightmapColGeom->BlendCount());
+
+  // Test heightmap visual
+  auto heightmapVis = link->VisualByName("heightmap_vis");
+  ASSERT_NE(nullptr, heightmapVis);
+  ASSERT_NE(nullptr, heightmapVis->Geom());
+  EXPECT_EQ(sdf::GeometryType::HEIGHTMAP, heightmapVis->Geom()->Type());
+  auto heightmapVisGeom = heightmapVis->Geom()->HeightmapShape();
+  ASSERT_NE(nullptr, heightmapVisGeom);
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/heightmap.png", heightmapVisGeom->Uri());
+  EXPECT_EQ(ignition::math::Vector3d(500, 500, 100), heightmapVisGeom->Size());
+  EXPECT_EQ(ignition::math::Vector3d(1, 2, 3), heightmapVisGeom->Position());
+  EXPECT_EQ(3u, heightmapVisGeom->TextureCount());
+  EXPECT_EQ(2u, heightmapVisGeom->BlendCount());
+
+  auto texture0 = heightmapVisGeom->TextureByIndex(0u);
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/diffuse0.png", texture0->Diffuse());
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/normal0.png", texture0->Normal());
+  EXPECT_DOUBLE_EQ(5.0, texture0->Size());
+
+  auto texture1 = heightmapVisGeom->TextureByIndex(1u);
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/diffuse1.png", texture1->Diffuse());
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/normal1.png", texture1->Normal());
+  EXPECT_DOUBLE_EQ(10.0, texture1->Size());
+
+  auto texture2 = heightmapVisGeom->TextureByIndex(2u);
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/diffuse2.png", texture2->Diffuse());
+  EXPECT_EQ("https://fuel.ignitionrobotics.org/1.0/an_org/models/a_model/"
+      "materials/textures/normal2.png", texture2->Normal());
+  EXPECT_DOUBLE_EQ(20.0, texture2->Size());
+
+  auto blend0 = heightmapVisGeom->BlendByIndex(0u);
+  EXPECT_DOUBLE_EQ(15.0, blend0->MinHeight());
+  EXPECT_DOUBLE_EQ(5.0, blend0->FadeDistance());
+
+  auto blend1 = heightmapVisGeom->BlendByIndex(1u);
+  EXPECT_DOUBLE_EQ(30.0, blend1->MinHeight());
+  EXPECT_DOUBLE_EQ(10.0, blend1->FadeDistance());
 }
