8d51f85806229c43914cf59e63a493174cb205f5
Michael Carroll <michael@openrobotics.org>
2020-11-10 12:18:57 -0600

    Add copyright and define guards to test_util
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- test/test_util.hh
+++ test/test_util.hh
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef SDF_TEST_UTIL_HH_
+#define SDF_TEST_UTIL_HH_
+
 #include "sdf/Filesystem.hh"
 
 #include "test_config.h"
@@ -45,5 +64,8 @@ namespace sdf
 
       return false;
     }
-  }
-}
+  }  // namespace testing
+}  // namespace sdf
+
+#endif  // SDF_TEST_UTIL_HH_
+
