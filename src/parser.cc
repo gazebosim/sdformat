e8b30f0bca75c2861b0b21ca042706cc3c117b22
Addisu Z. Taddese <addisu@openrobotics.org>
2021-01-14 17:57:42 -0600

    Allow files paths for include URIs (#448)
    //include/uri used to only take directories that contain a model.config file and the sdformat file. However, allowing //include/uri to take file names can be supported without loss of functionality.
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- src/parser.cc
+++ src/parser.cc
@@ -978,24 +978,27 @@ bool readXml(tinyxml2::XMLElement *_xml, ElementPtr _sdf, Errors &_errors)
           }
           else
           {
-            if (!sdf::filesystem::is_directory(modelPath))
+            if (sdf::filesystem::is_directory(modelPath))
             {
-              _errors.push_back({ErrorCode::DIRECTORY_NONEXISTANT,
-                  "Directory doesn't exist[" + modelPath + "]"});
-              continue;
+              // Get the model.config filename
+              filename = getModelFilePath(modelPath);
+
+              if (filename.empty())
+              {
+                _errors.push_back({ErrorCode::URI_LOOKUP,
+                    "Unable to resolve uri[" + uri + "] to model path [" +
+                    modelPath + "] since it does not contain a model.config " +
+                    "file."});
+                continue;
+              }
             }
-          }
-
-          // Get the config.xml filename
-          filename = getModelFilePath(modelPath);
-
-          if (filename.empty())
-          {
-            _errors.push_back({ErrorCode::URI_LOOKUP,
-                "Unable to resolve uri[" + uri + "] to model path [" +
-                modelPath + "] since it does not contain a model.config " +
-                "file."});
-            continue;
+            else
+            {
+              // This is a file path and since sdf::findFile returns an empty
+              // string if the file doesn't exist, we don't have to check for
+              // existence again here.
+              filename = modelPath;
+            }
           }
         }
         else
--- test/integration/includes.cc
+++ test/integration/includes.cc
@@ -60,11 +60,11 @@ TEST(IncludesTest, Includes)
 
   ASSERT_NE(nullptr, root.Element());
   EXPECT_EQ(worldFile, root.Element()->FilePath());
-  EXPECT_EQ("1.6", root.Element()->OriginalVersion());
+  EXPECT_EQ("1.8", root.Element()->OriginalVersion());
 
   const sdf::World *world = root.WorldByIndex(0);
   ASSERT_NE(nullptr, world);
-  EXPECT_EQ("1.6", world->Element()->OriginalVersion());
+  EXPECT_EQ("1.8", world->Element()->OriginalVersion());
 
   // Actors
   EXPECT_EQ(2u, world->ActorCount());
@@ -149,7 +149,7 @@ TEST(IncludesTest, Includes)
   EXPECT_EQ("", pointLight1->PoseRelativeTo());
 
   // Models
-  EXPECT_EQ(2u, world->ModelCount());
+  EXPECT_EQ(3u, world->ModelCount());
   EXPECT_FALSE(world->ModelNameExists(""));
 
   // Model without overrides
@@ -209,6 +209,19 @@ TEST(IncludesTest, Includes)
   EXPECT_EQ("", model1->PoseRelativeTo());
   ASSERT_NE(nullptr, model1->Element());
   EXPECT_TRUE(model1->Element()->HasElement("plugin"));
+
+  const sdf::Model *model2 = world->ModelByIndex(2);
+  ASSERT_NE(nullptr, model2);
+  EXPECT_EQ("test_model_with_file", model2->Name());
+  EXPECT_FALSE(model2->Static());
+  EXPECT_EQ(1u, model2->LinkCount());
+  ASSERT_NE(nullptr, model2->LinkByIndex(0));
+  ASSERT_NE(nullptr, model2->LinkByName("link"));
+  EXPECT_EQ(model2->LinkByName("link")->Name(), model2->LinkByIndex(0)->Name());
+  EXPECT_EQ(nullptr, model2->LinkByIndex(1));
+  EXPECT_TRUE(model2->LinkNameExists("link"));
+  EXPECT_FALSE(model2->LinkNameExists("coconut"));
+  EXPECT_EQ("1.6", model2->Element()->OriginalVersion());
 }
 
 //////////////////////////////////////////////////
