50e991a2b4f06dfbcdf751dfde25f6632b8a9570
Eric Cousineau <eric.cousineau@tri.global>
2021-02-01 16:48:45 -0500

    Root: Fix minor grammar error in error message (#478)
    Signed-off-by: Eric Cousineau <eric.cousineau@tri.global>

--- src/Root.cc
+++ src/Root.cc
@@ -180,7 +180,7 @@ Errors Root::LoadSdfString(const std::string &_sdf)
   if (!readString(_sdf, sdfParsed, errors))
   {
     errors.push_back(
-        {ErrorCode::STRING_READ, "Unable to SDF string: " + _sdf});
+        {ErrorCode::STRING_READ, "Unable to read SDF string: " + _sdf});
     return errors;
   }
 
