1333f59d7c32469316861b2a2f9c26076c3598c5
Jenn Nguyen <jenn@openrobotics.org>
2021-03-18 16:10:22 -0700

    Unflatten flattened models (#504)
    * initial unnesting conversion
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * unnested links & added integration test
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * updated nested model test
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * strip prefix for //link/<element>/pose & //joint/sensor/pose
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * strip prefix for //model/include/pose & //model/gripper
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * Removed use of copied element to unnest
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * Fixed elem iteration (removed DeepClone)
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * changed unnest to unflatten & added expectations back
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * updated tests
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * readded some tests back
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>
    
    * renamed function & strip //gripper/@name if prefix present
    
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>

--- src/Converter.hh
+++ src/Converter.hh
@@ -20,6 +20,7 @@
 #include <tinyxml2.h>
 
 #include <string>
+#include <tuple>
 
 #include <sdf/sdf_config.h>
 #include "sdf/system_util.hh"
@@ -102,6 +103,20 @@ namespace sdf
     /// \param[in] _removeElem The element to remove.
     private: static void Remove(tinyxml2::XMLElement *_elem,
                                 tinyxml2::XMLElement *_removeElem);
+
+    /// \brief Unflatten an element (conversion from SDFormat <= 1.7 to 1.8)
+    /// \param[in] _elem The element to unflatten
+    private: static void Unflatten(tinyxml2::XMLElement *_elem);
+
+    /// \brief Finds all elements related to the unflattened model
+    /// \param[in] _elem The element to unflatten
+    /// \param[in] _newModel The new unflattened model element
+    /// \param[in] _childNameIdx The beginning index of child element names
+    /// (e.g., in newModelName::childName then _childNameIdx = 14)
+    /// \return True if unflattened new model elements
+    private: static bool FindNewModelElements(tinyxml2::XMLElement *_elem,
+                                              tinyxml2::XMLElement *_newModel,
+                                              const size_t &_childNameIdx);
 
     private: static const char *GetValue(const char *_valueElem,
                                          const char *_valueAttr,
