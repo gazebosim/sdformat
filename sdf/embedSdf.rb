1a779837277034ec45f67cc9f1ea9a47bf4bfd9a
Aaron Chong <aaronchongth@gmail.com>
2021-06-11 11:45:29 +0800

    Copy spec 1.8 to 1.9 (#568)
    * Copy spec 1.8 to 1.9
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Merging in changes from #570
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Fixing version in root
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- sdf/embedSdf.rb
+++ sdf/embedSdf.rb
@@ -2,11 +2,11 @@
 
 # The list of supported SDF specification versions. This will let us drop
 # versions without removing the directories.
-supportedSdfVersions = ['1.8', '1.7', '1.6', '1.5', '1.4', '1.3', '1.2']
+supportedSdfVersions = ['1.9', '1.8', '1.7', '1.6', '1.5', '1.4', '1.3', '1.2']
 
 # The list of supported SDF conversions. This list includes versions that
 # a user can convert an existing SDF version to.
-supportedSdfConversions = ['1.8', '1.7', '1.6', '1.5', '1.4', '1.3']
+supportedSdfConversions = ['1.9', '1.8', '1.7', '1.6', '1.5', '1.4', '1.3']
 
 puts %q!
 #include "EmbeddedSdf.hh"
