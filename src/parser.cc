a9e4abe531abd11c50e2b9f11393b305ab4aba05
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-06-17 20:58:46 +0200

    Fix flattening logic for composed names in nested models
    When nested models use names composed by several elements
    (i.e: my_model::link) these were not converted by the
    flattening logic inside parser.cc. The change makes the
    logic to work with composed names.
    
    Added a test to check that this is indeed working as
    expected.
    
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- src/parser.cc
+++ src/parser.cc
@@ -1117,6 +1117,35 @@ void addNestedModel(ElementPtr _sdf, ElementPtr _includeSDF)
                      "'" + name_with_nested_prefix + "'");
     replace_all(str, ">" + old_name + "<",
                      ">" + name_with_nested_prefix + "<");
+    // Deal with nested model inside other nested model and already with
+    // '::component::' entries in the name
+    // Case A. ::component1::iter.first::component2
+    replace_all(str, "::" + old_name + "::",
+                     "::" + name_with_nested_prefix + "::");
+    // Case B. >iter.first::component
+    replace_all(str, ">" + old_name + "::",
+                     ">" + name_with_nested_prefix + "::");
+    // Case C.::component::iter.first<
+    replace_all(str, "::" + old_name + "<",
+                     "::" + name_with_nested_prefix + "<");
+    // remove duplicate prefixes introduced by the logic above (cases
+    // A,B,C) if the full name (iter.second) was used in the original SDF: (i.e
+    // iter.second::component)
+    std::string::size_type found = name_with_nested_prefix.find(old_name);
+    if (found != std::string::npos)
+    {
+      std::string nested_prefix = name_with_nested_prefix;
+      nested_prefix.erase(found, old_name.length());
+      replace_all(str, nested_prefix + nested_prefix, nested_prefix);
+    }
+    else
+    {
+      // should never happen since name_with_nested_prefix =
+      // nested model name + old_name
+      sdferr << "Nested name '" << name_with_nested_prefix <<
+                "' does not contain the plain name of the element '" <<
+                old_name << "'. Please report an issue. \n";
+    }
   }
 
   _includeSDF->ClearElements();
--- test/integration/nested_model.cc
+++ test/integration/nested_model.cc
@@ -24,6 +24,8 @@
 
 #include "sdf/sdf.hh"
 
+#include "test_config.h"
+
 ////////////////////////////////////////
 // Test parsing nested model with joint
 TEST(NestedModel, NestedModel)
@@ -238,4 +240,53 @@ TEST(NestedModel, State)
   EXPECT_TRUE(nestedLinkStateElem->HasElement("wrench"));
   EXPECT_EQ(nestedLinkStateElem->Get<ignition::math::Pose3d>("wrench"),
     ignition::math::Pose3d(0, 0, 0, 0, 0, 0));
+}
+
+////////////////////////////////////////
+// Test parsing a include element that has a pose element and includes a
+// submodel
+TEST(NestedModel, IncludeFlatteningNames)
+{
+  const std::string MODEL_PATH =
+    sdf::filesystem::append(PROJECT_SOURCE_PATH, "test", "integration",
+                            "model", "nested_names_test");
+
+  std::ostringstream stream;
+  std::string version = SDF_VERSION;
+  stream
+    << "<sdf version='" << version << "'>"
+    << "<model name='top_level_model'>"
+    << "  <include>"
+    << "    <uri>" + MODEL_PATH +"</uri>"
+    << "  </include>"
+    << "</model>"
+    << "</sdf>";
+
+  sdf::SDFPtr sdfParsed(new sdf::SDF());
+  sdf::init(sdfParsed);
+  ASSERT_TRUE(sdf::readString(stream.str(), sdfParsed));
+
+  sdf::ElementPtr modelElem = sdfParsed->Root()->GetElement("model");
+  EXPECT_EQ(modelElem->Get<std::string>("name"), "top_level_model");
+
+  sdf::ElementPtr linkElem = modelElem->GetElement("link");
+  EXPECT_EQ(linkElem->Get<std::string>("name"), "main_model_prefix::frame");
+
+  sdf::ElementPtr jointElem = modelElem->GetElement("joint");
+  EXPECT_EQ(jointElem->Get<std::string>("name"), "main_model_prefix::joint1");
+  EXPECT_EQ(jointElem->Get<std::string>("parent"),
+    "main_model_prefix::subnested_model");
+  EXPECT_EQ(jointElem->Get<std::string>("child"),
+    "main_model_prefix::subnested_model::link1") <<
+    "Flattening logic for nested models failed for case B (check parser.cc)";
+
+  sdf::ElementPtr joint2Elem = jointElem->GetNextElement("joint");
+  EXPECT_EQ(joint2Elem->Get<std::string>("name"), "main_model_prefix::joint2");
+  EXPECT_EQ(joint2Elem->Get<std::string>("parent"),
+    "main_model_prefix::subnested_model::link1") <<
+    "Flattening logic for nested models failed for case A (check parser.cc)" +
+    "duplicates";
+  EXPECT_EQ(joint2Elem->Get<std::string>("child"),
+    "other_prefix::main_model_prefix::subnested_model") <<
+    "Flattening logic for nested models failed for case C (check parser.cc)";
 }
