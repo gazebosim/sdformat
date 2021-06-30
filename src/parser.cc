e17aef4392b36019c4f64337ca672864857d5676
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-06-30 14:34:15 +0200

    Fix flattening logic for nested model names (sdf6) (#597)
    * Fix flattening logic for composed names in nested models
    
    When nested models use names composed by several elements
    (i.e: my_model::link) these were not converted by the
    flattening logic inside parser.cc. The change makes the
    logic to work with composed names.
    
    Added a test to check that this is indeed working as
    expected.
    
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- src/parser.cc
+++ src/parser.cc
@@ -1074,11 +1074,18 @@ void addNestedModel(ElementPtr _sdf, ElementPtr _includeSDF)
   std::string modelName = modelPtr->Get<std::string>("name");
   while (elem)
   {
-    if (elem->GetName() == "link")
+    // protect elements that should not change the name attribute
+    // i.e: plugin
+    if ((elem->GetName() == "link") || (elem->GetName() == "joint") ||
+        (elem->GetName() == "model"))
     {
       std::string elemName = elem->Get<std::string>("name");
       std::string newName =  modelName + "::" + elemName;
       replace[elemName] = newName;
+    }
+
+    if (elem->GetName() == "link")
+    {
       if (elem->HasElementDescription("pose"))
       {
         ignition::math::Pose3d offsetPose =
@@ -1092,11 +1099,6 @@ void addNestedModel(ElementPtr _sdf, ElementPtr _includeSDF)
     }
     else if (elem->GetName() == "joint")
     {
-      // for joints, we need to
-      //   prefix name like we did with links, and
-      std::string elemName = elem->Get<std::string>("name");
-      std::string newName =  modelName + "::" + elemName;
-      replace[elemName] = newName;
       //   rotate the joint axis because they are model-global
       if (elem->HasElement("axis"))
       {
@@ -1113,12 +1115,18 @@ void addNestedModel(ElementPtr _sdf, ElementPtr _includeSDF)
   for (std::map<std::string, std::string>::iterator iter = replace.begin();
        iter != replace.end(); ++iter)
   {
-    replace_all(str, std::string("\"") + iter->first + "\"",
-                std::string("\"") + iter->second + "\"");
-    replace_all(str, std::string("'") + iter->first + "'",
-                std::string("'") + iter->second + "'");
-    replace_all(str, std::string(">") + iter->first + "<",
-                std::string(">") + iter->second + "<");
+    std::string oldName(iter->first);
+    std::string nameWithNestedPrefix(iter->second);
+    replace_all(str, std::string("\"") + oldName + "\"",
+                     std::string("\"") + nameWithNestedPrefix + "\"");
+    replace_all(str, std::string("'") + oldName + "'",
+                     std::string("'") + nameWithNestedPrefix + "'");
+    replace_all(str, std::string(">") + oldName + "<",
+                     std::string(">") + nameWithNestedPrefix + "<");
+    // Deal with nested model inside other nested model and already with
+    // ::namespace:: entries in the name
+    replace_all(str, std::string(">") + oldName + "::",
+                     std::string(">") + nameWithNestedPrefix + "::");
   }
 
   _includeSDF->ClearElements();
