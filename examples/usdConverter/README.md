5e13b49f68e3681ea6ded9f6872424ca54de6d24
Ashton Larkin <42042756+adlarkin@users.noreply.github.com>
2022-01-06 16:56:49 -0700

    make SDF to USD a separate component of sdformat
    Signed-off-by: Ashton Larkin <42042756+adlarkin@users.noreply.github.com>

---
+++ examples/usdConverter/README.md
@@ -0,0 +1,30 @@
+# Converting between SDF and USD
+
+This example shows how a world in a SDF file can be converted to [USD](https://graphics.pixar.com/usd/release/index.html).
+
+## Requirements
+
+You will need all of the dependencies for sdformat, along with the following additional dependencies:
+* USD: [installation instructions](https://github.com/PixarAnimationStudios/USD/blob/release/README.md#getting-and-building-the-code)
+* [ignition-common4](https://github.com/ignitionrobotics/ign-common)
+
+## Setup
+
+Build sdformat, and then run the following commands to build the example (run these commands from this example directory):
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+You should now have an executable named `sdf2usd`, which can be used to convert a SDF world file to a USD file.
+The following command converts the example `shapes.sdf` file to its USD representation, stored in a file called `shapes.usda` (run this command from the `build` directory):
+```bash
+./sdf2usd ../shapes.sdf shapes.usda
+```
+
+You can now view the contents of the generated USD file with `usdview` (this should have been installed when setting up the USD dependency):
+```
+usdview shapes.usda
+```
---
+++ usd/src/cmd/sdf2usd.cc
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <string.h>
+
+#include <ignition/utils/cli/CLI.hpp>
+#include <pxr/usd/usd/stage.h>
+
+#include "sdf/sdf.hh"
+#include "sdf/usd/World.hh"
+
+//////////////////////////////////////////////////
+/// \brief Enumeration of available commands
+enum class Command
+{
+  kNone,
+};
+
+//////////////////////////////////////////////////
+/// \brief Structure to hold all available topic options
+struct Options
+{
+  /// \brief Command to execute
+  Command command{Command::kNone};
+
+  /// \brief input filename
+  std::string inputFilename{"input.sdf"};
+
+  /// \brief output filename
+  std::string outputFilename{"output.sdf"};
+};
+
+void runCommand(const Options &_opt)
+{
+  sdf::Root root;
+  auto errors = root.Load(_opt.inputFilename);
+  if (!errors.empty())
+  {
+    std::cerr << "Errors encountered:\n";
+    for (const auto &e : errors)
+      std::cout << e << "\n";
+    exit(-2);
+  }
+
+  // only support SDF files with exactly 1 world for now
+  if (root.WorldCount() != 1u)
+  {
+    std::cerr << _opt.inputFilename << " does not have exactly 1 world\n";
+    exit(-3);
+  }
+
+  auto world = root.WorldByIndex(0u);
+  if (!world)
+  {
+    std::cerr << "Error retrieving the world from "
+              << _opt.inputFilename << "\n";
+    exit(-4);
+  }
+
+  auto stage = pxr::UsdStage::CreateInMemory();
+
+  const auto worldPath = std::string("/" + world->Name());
+  if (!usd::ParseSdfWorld(*world, stage, worldPath))
+  {
+    std::cerr << "Error parsing world [" << world->Name() << "]\n";
+    exit(-5);
+  }
+
+  if (!stage->GetRootLayer()->Export(_opt.outputFilename))
+  {
+    std::cerr << "Issue saving USD to " << _opt.outputFilename << "\n";
+    exit(-6);
+  }
+}
+
+void addFlags(CLI::App &_app)
+{
+  auto opt = std::make_shared<Options>();
+
+  _app.add_option("-i,--input",
+    opt->inputFilename,
+    "Input filename");
+
+  _app.add_option("-o,--output",
+    opt->outputFilename,
+    "Output filename");
+
+  _app.callback([&_app, opt](){
+    runCommand(*opt);
+  });
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char** argv)
+{
+  CLI::App app{"Sdf format converter"};
+
+  app.set_help_all_flag("--help-all", "Show all help");
+
+  app.add_flag_callback("--version", [](){
+    std::cout << strdup(SDF_VERSION_FULL) << std::endl;
+    throw CLI::Success();
+  });
+
+  addFlags(app);
+  CLI11_PARSE(app, argc, argv);
+
+  return 0;
+}
