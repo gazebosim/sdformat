bdd7829405c0eb2f3bcf7c29b1a8a5b28ce52f34
ahcorde <ahcorde@gmail.com>
2022-01-07 20:56:06 +0100

    udpate ci
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+BUILD_DIR=`pwd`
+
 cd /tmp
 mkdir usd_binaries
 cd usd_binaries
@@ -11,3 +13,4 @@ apt install p7zip-full -y
 wget https://developer.download.nvidia.com/USD/usd_binaries/21.05/usd-21-05-usd-linux64_py36-centos_release.7z
 
 7z x usd-21-05-usd-linux64_py36-centos_release.7z
+cd $BUILD_DIR
