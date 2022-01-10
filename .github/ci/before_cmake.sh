2959031061e118d7e8f133b106da2a3e376c5004
ahcorde <ahcorde@gmail.com>
2022-01-10 15:34:42 +0100

    update ci
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -8,9 +8,38 @@ cd /tmp
 mkdir usd_binaries
 cd usd_binaries
 
-apt install p7zip-full -y
+apt-get install libboost-all-dev libtbb-dev p7zip-full -y
 
-wget https://developer.download.nvidia.com/USD/usd_binaries/21.05/usd-21-05-usd-linux64_py36-centos_release.7z
+wget https://github.com/PixarAnimationStudios/USD/archive/refs/tags/v21.11.zip
+unzip v21.11.zip
+sed -i '2059 i \ \ \ \ requiredDependencies.remove(BOOST)' USD-21.11/build_scripts/build_usd.py
+cd USD-21.11
+mkdir build
+cd build
 
-7z x usd-21-05-usd-linux64_py36-centos_release.7z
+cmake -DCMAKE_INSTALL_PREFIX="/tmp/USD" -DCMAKE_PREFIX_PATH="/tmp/USD" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPXR_PREFER_SAFETY_OVER_SPEED=ON \
+  -DPXR_ENABLE_PYTHON_SUPPORT=OFF \
+  -DBUILD_SHARED_LIBS=ON  \
+  -DTBB_USE_DEBUG_BUILD=OFF  \
+  -DPXR_BUILD_DOCUMENTATION=OFF  \
+  -DPXR_BUILD_TESTS=OFF  \
+  -DPXR_BUILD_EXAMPLES=OFF  \
+  -DPXR_BUILD_TUTORIALS=OFF  \
+  -DPXR_BUILD_USD_TOOLS=OFF  \
+  -DPXR_BUILD_IMAGING=OFF  \
+  -DPXR_BUILD_USD_IMAGING=OFF  \
+  -DPXR_BUILD_USDVIEW=OFF  \
+  -DPXR_BUILD_ALEMBIC_PLUGIN=OFF  \
+  -DPXR_BUILD_DRACO_PLUGIN=OFF  \
+  -DPXR_ENABLE_MATERIALX_SUPPORT=OFF  \
+  -DBoost_NO_BOOST_CMAKE=On  \
+  -DBoost_NO_SYSTEM_PATHS=True \
+  -DBoost_INCLUDE_DIR=/usr/include  \
+  -DBoost_NO_BOOST_CMAKE=FALS \
+  ..
+
+make install
+
 cd $BUILD_DIR
