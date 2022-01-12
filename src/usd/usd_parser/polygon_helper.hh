774a5ec241c9487031ea20a30604bae325dd09ab
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-12 10:10:57 +0000

    polygon triangulation using the fan-triangulation algorithm
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- src/usd/usd_parser/polygon_helper.hh
+++ src/usd/usd_parser/polygon_helper.hh
@@ -20,8 +20,16 @@
 
 #include <pxr/base/gf/vec3d.h>
 #include <pxr/base/vt/array.h>
+
 namespace usd
 {
+  ///\brief Converts a vertex indicies of a polygon mesh to a vertex indicies of a triangle mesh.
+  ///\details This uses the fan-triangulating algorithm, so it only works if all polygons in
+  /// in the mesh are convex.
+  ///\param _faceVertexIndices A flat list of vertex indicies of a polygon mesh.
+  ///\param _faceVertexCounts A list containing the number of vertices for each face of the mesh.
+  ///\param _points A list of vertices.
+  ///\return A flat list of vertex indicies, with each face converted to one or more triangles.
   std::vector<unsigned int> PolygonToTriangles(
     pxr::VtIntArray &_faceVertexIndices,
     pxr::VtIntArray &_faceVertexCounts,
