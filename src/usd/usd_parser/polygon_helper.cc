aa1333d858cddbe8c6e9a972eaf45dc0ef92184d
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-18 20:58:31 +0800

    USD -> SDF: polygon triangulation (#819)
    * polygon triangulation using the fan-triangulation algorithm
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>
    
    * remove cgal dep
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>
    
    * add todo
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>
    
    * fix xformOp:transform parsing (#822)
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>
    
    Co-authored-by: ahcorde <ahcorde@gmail.com>

--- src/usd/usd_parser/polygon_helper.cc
+++ src/usd/usd_parser/polygon_helper.cc
@@ -17,77 +17,41 @@
 
 #include "polygon_helper.hh"
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_3.h>
-#include <CGAL/Triangulation_vertex_base_with_info_3.h>
-#include <CGAL/Random.h>
 #include <vector>
 #include <cassert>
-
-typedef CGAL::Exact_predicates_inexact_constructions_kernel          K;
-typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned int, K> Vb;
-typedef CGAL::Delaunay_triangulation_cell_base_3<K>                  Cb;
-typedef CGAL::Triangulation_data_structure_3<Vb, Cb>                 Tds;
-typedef CGAL::Delaunay_triangulation_3<K, Tds>                       Delaunay;
-typedef Delaunay::Point                                              Point;
 
 namespace usd
 {
   std::vector<unsigned int> PolygonToTriangles(
-    pxr::VtIntArray &_faceVertexIndices,
-    pxr::VtIntArray &_faceVertexCounts,
-    pxr::VtArray<pxr::GfVec3f> &_points)
+      pxr::VtIntArray &_faceVertexIndices,
+      pxr::VtIntArray &_faceVertexCounts,
+      pxr::VtArray<pxr::GfVec3f> &_points)
   {
-    std::vector<unsigned int> indices;
-
-    unsigned int indexVertex = 0;
-    for (unsigned int i = 0; i < _faceVertexCounts.size(); ++i)
+    // TODO: Use more robust algorithms.
+    // For reference, blender supports "ear-clipping", and "Beauty".
+    // ref: https://blender.stackexchange.com/questions/215553/what-algorithm-is-used-for-beauty-triangulation
+    // ref: https://en.wikipedia.org/wiki/Polygon_triangulation
+    size_t count = 0;
+    for (const auto &vCount : _faceVertexCounts)
     {
-      if (_faceVertexCounts[i] == 3)
-      {
-        unsigned int j = indexVertex;
-        unsigned int indexVertexStop = indexVertex + 3;
-        for (; j < indexVertexStop; ++j)
-        {
-          indices.emplace_back(_faceVertexIndices[j]);
-          ++indexVertex;
-        }
-      }
-      else
-      {
-        std::vector< std::pair<Point, unsigned> > P;
-        for (int j = 0; j < _faceVertexCounts[i]; ++j)
-        {
-          pxr::GfVec3f & _p = _points[_faceVertexIndices[indexVertex]];
-          P.push_back(
-            std::make_pair(
-              Point(_p[0], _p[1], _p[2]),
-              _faceVertexIndices[indexVertex]));
-          ++indexVertex;
-        }
-        Delaunay triangulation(P.begin(), P.end());
-
-        bool goodTriangle = true;
-        for(Delaunay::Finite_facets_iterator fit = triangulation.finite_facets_begin();
-            fit != triangulation.finite_facets_end(); ++fit)
-        {
-          auto &face = fit;
-          goodTriangle = false;
-        }
-
-        if (goodTriangle)
-        {
-          for(Delaunay::Finite_facets_iterator fit = triangulation.finite_facets_begin();
-              fit != triangulation.finite_facets_end(); ++fit)
-          {
-            auto &face = fit;
-            indices.emplace_back(face->first->vertex(0)->info());
-            indices.emplace_back(face->first->vertex(1)->info());
-            indices.emplace_back(face->first->vertex(2)->info());
-          }
-        }
-      }
+      count += vCount - 2;
     }
-    return indices;
+    std::vector<unsigned int> triangles;
+    triangles.reserve(count * 3);
+
+    size_t cur = 0;
+    for (const auto &vCount : _faceVertexCounts)
+    {
+      for (size_t i = cur + 2; i < cur + vCount; i++)
+      {
+        triangles.emplace_back(_faceVertexIndices[cur]);
+        triangles.emplace_back(_faceVertexIndices[i - 1]);
+        triangles.emplace_back(_faceVertexIndices[i]);
+      }
+      cur += vCount;
+    }
+    assert(triangles.size() == count * 3);
+
+    return triangles;
   }
 }
