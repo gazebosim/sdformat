851649b4db395da01b34660369269b980b4ef0d6
Ashton Larkin <42042756+adlarkin@users.noreply.github.com>
2022-02-10 13:00:14 -0700

    remove EXTERNAL_ERROR type
    Signed-off-by: Ashton Larkin <42042756+adlarkin@users.noreply.github.com>

--- include/sdf/Error.hh
+++ include/sdf/Error.hh
@@ -147,9 +147,6 @@ namespace sdf
     /// \brief Merge include is unspported for the type of entity being
     /// included, or the custom parser does not support merge includes.
     MERGE_INCLUDE_UNSUPPORTED,
-
-    /// \brief A generic error that is not an sdf error.
-    EXTERNAL_ERROR,
   };
 
   class SDFORMAT_VISIBLE Error
