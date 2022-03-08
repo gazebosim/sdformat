0f34f619211a65197d70d43a9da96c62ed11d2d2
ahcorde <ahcorde@gmail.com>
2022-03-08 16:59:06 +0100

    Added materials tests
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- usd/src/usd_parser/USDData_TEST.cc
+++ usd/src/usd_parser/USDData_TEST.cc
@@ -18,6 +18,7 @@
 #include <gtest/gtest.h>
 
 #include <ignition/common/Filesystem.hh>
+#include <ignition/common/Util.hh>
 
 #include <ignition/utilities/ExtraTestMacros.hh>
 
@@ -27,6 +28,9 @@
 #include "test_config.h"
 #include "test_utils.hh"
 
+#include <sdf/Material.hh>
+#include <sdf/Pbr.hh>
+
 /////////////////////////////////////////////////
 TEST(USDData, Constructor)
 {
@@ -35,14 +39,68 @@ TEST(USDData, Constructor)
   sdf::usd::UsdErrors errors = data.Init();
   EXPECT_EQ(1u, errors.size());
 
+  // Add test/usd directory to find some resources
+  auto systemPaths = ignition::common::systemPaths();
+  systemPaths->AddFilePaths(sdf::testing::TestFile("usd"));
+
   // Open a valid USD file
   {
     std::string filename = sdf::testing::TestFile("usd", "upAxisZ.usda");
     sdf::usd::USDData usdData(filename);
     EXPECT_EQ(0u, usdData.Init().size());
     EXPECT_EQ(0u, usdData.ParseMaterials().size());
+
     EXPECT_EQ(1u, usdData.AllReferences().size());
     EXPECT_EQ(2u, usdData.Models().size());
+    EXPECT_EQ(7u, usdData.Materials().size());
+
+    auto materials = usdData.Materials();
+
+    auto material0 = materials["Material_0"];
+    EXPECT_EQ(ignition::math::Color(0.8, 0.8, 0.8), material0.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material0.Emissive());
+
+    auto material1 = materials["Material_1"];
+    EXPECT_EQ(ignition::math::Color(1, 0, 0), material1.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material1.Emissive());
+
+    auto material2 = materials["Material_2"];
+    EXPECT_EQ(ignition::math::Color(0, 1, 0), material2.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material2.Emissive());
+
+    auto material3 = materials["Material_3"];
+    EXPECT_EQ(ignition::math::Color(0, 0, 1), material3.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material3.Emissive());
+
+    auto material4 = materials["Material_4"];
+    EXPECT_EQ(ignition::math::Color(1, 1, 0), material4.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material4.Emissive());
+
+    auto material5 = materials["Material_5"];
+    EXPECT_EQ(ignition::math::Color(1, 0, 1), material5.Diffuse());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), material5.Emissive());
+
+    auto materialTextures = materials["Material_textures"];
+    const auto * pbr = materialTextures.PbrMaterial();
+    ASSERT_TRUE(pbr);
+    const auto * workflow = pbr->Workflow(sdf::PbrWorkflowType::METAL);
+    ASSERT_TRUE(workflow);
+    EXPECT_EQ(ignition::math::Color(1, 1, 1), materialTextures.Diffuse());
+    EXPECT_EQ(
+      "materials/textures/FANS_Albedo.png",
+      workflow->AlbedoMap());
+    EXPECT_EQ(ignition::math::Color(0, 0, 0), materialTextures.Emissive());
+    EXPECT_DOUBLE_EQ(0.5, workflow->Metalness());
+    EXPECT_EQ(
+      "materials/textures/FANS_Metalness.png",
+      workflow->MetalnessMap());
+    EXPECT_DOUBLE_EQ(0.5, workflow->Roughness());
+    EXPECT_EQ(
+      "materials/textures/FANS_Roughness.png",
+      workflow->RoughnessMap());
+    EXPECT_EQ(
+      "materials/textures/FANS_Normal.png",
+      workflow->NormalMap());
 
     // Find a path inside the stage
     auto boxStage = usdData.FindStage("box");
@@ -54,6 +112,10 @@ TEST(USDData, Constructor)
     auto invalidStage = usdData.FindStage("invalid");
     EXPECT_EQ("", invalidStage.first);
     EXPECT_EQ(nullptr, invalidStage.second);
+
+    // Remove copied materials
+    ignition::common::removeAll(
+      ignition::common::joinPaths(ignition::common::cwd(), "materials"));
   }
 }
 
