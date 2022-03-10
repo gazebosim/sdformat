b7447df461616b7ffc964adc5e46f83fa264bef7
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-03-10 09:59:58 +0100

    USD -> SDF: Added cmd line tool (#862)
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    Co-authored-by: Ashton Larkin <42042756+adlarkin@users.noreply.github.com>

---
+++ usd/src/cmd/usd2sdf.cc
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <string>
+
+#include <ignition/utils/cli/CLI.hpp>
+
+#include "sdf/config.hh"
+
+//////////////////////////////////////////////////
+/// \brief Enumeration of available commands
+enum class Command
+{
+  kNone,
+};
+
+//////////////////////////////////////////////////
+/// \brief Structure to hold all filenames
+struct Options
+{
+  /// \brief Command to execute
+  Command command{Command::kNone};
+
+  /// \brief input filename
+  std::string inputFilename{"input.usd"};
+
+  /// \brief output filename
+  std::string outputFilename{"output.sdf"};
+};
+
+void runCommand(const Options &/*_opt*/)
+{
+  // TODO(ahcorde): Call here the USD to SDF converter code
+  std::cerr << "USD to SDF conversion is not implemented yet.\n";
+}
+
+void addFlags(CLI::App &_app)
+{
+  auto opt = std::make_shared<Options>();
+
+  _app.add_option("input",
+    opt->inputFilename,
+    "Input filename. Defaults to input.usd unless otherwise specified.");
+
+  _app.add_option("output",
+    opt->outputFilename,
+    "Output filename. Defaults to output.sdf unless otherwise specified.");
+
+  _app.callback([&_app, opt](){
+    runCommand(*opt);
+  });
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char** argv)
+{
+  CLI::App app{"USD to SDF converter"};
+
+  app.set_help_all_flag("--help-all", "Show all help");
+
+  app.add_flag_callback("--version", [](){
+    std::cout << SDF_VERSION_FULL << std::endl;
+    throw CLI::Success();
+  });
+
+  addFlags(app);
+  CLI11_PARSE(app, argc, argv);
+
+  return 0;
+}
