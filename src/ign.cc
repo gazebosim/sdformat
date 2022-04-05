013b15dfbe57442ccee7c97a651a41104f3b85ed
Aditya <aditya050995@gmail.com>
2022-04-05 15:52:39 -0700

    Print centre of mass, total mass, and pretty print MOI matrix
    Signed-off-by: Aditya <aditya050995@gmail.com>

--- src/ign.cc
+++ src/ign.cc
@@ -23,10 +23,14 @@
 
 #include "sdf/sdf_config.h"
 #include "sdf/Filesystem.hh"
+#include "sdf/Link.hh"
+#include "sdf/Model.hh"
 #include "sdf/Root.hh"
 #include "sdf/parser.hh"
 #include "sdf/PrintConfig.hh"
 #include "sdf/system_util.hh"
+
+#include "ignition/math/Inertial.hh"
 
 #include "FrameSemantics.hh"
 #include "ScopedGraph.hh"
@@ -264,6 +268,88 @@ extern "C" SDFORMAT_VISIBLE int cmdGraph(
     std::cerr << R"(Only "pose" and "frame" graph types are supported)"
               << std::endl;
   }
+
+  return 0;
+}
+
+//////////////////////////////////////////////////
+extern "C" SDFORMAT_VISIBLE int cmdInertialStats(
+    const char *_path)
+{
+  if (!sdf::filesystem::exists(_path))
+  {
+    std::cerr << "Error: File [" << _path << "] does not exist.\n";
+    return -1;
+  }
+
+  sdf::Root root;
+  if (!root.Load(_path).empty()) {
+    return -1;
+  }
+
+  const sdf::Model * model = root.Model();
+  if (!model){
+    return -1;
+  }
+
+  ignition::math::Inertiald totalInertial;
+
+  for (uint64_t i = 0; i < model->LinkCount(); i++) {
+    ignition::math::Pose3d linkPoseRelativeToModel;
+    auto errors = model->LinkByIndex(i)->SemanticPose().Resolve(linkPoseRelativeToModel, "__model__");
+
+    auto currentLinkInertial = model->LinkByIndex(i)->Inertial();
+    currentLinkInertial.SetPose(linkPoseRelativeToModel * currentLinkInertial.Pose());
+    totalInertial += currentLinkInertial;
+  }
+
+  auto totalMass = totalInertial.MassMatrix().Mass();
+  auto xCentreOfMass = totalInertial.Pose().Pos().X();
+  auto yCentreOfMass = totalInertial.Pose().Pos().Y();
+  auto zCentreOfMass = totalInertial.Pose().Pos().Z();
+
+  std::cout << "Inertial statistics for model: " << model->Name() << std::endl;
+  std::cout << "---" << std::endl;
+  std::cout << "Total mass of the model: " << totalMass << std::endl;
+  std::cout << "---" << std::endl;
+
+  std::cout << "Centre of mass in model frame: " << std::endl;
+  std::cout << "X: " << xCentreOfMass << std::endl;
+  std::cout << "Y: " << yCentreOfMass << std::endl;
+  std::cout << "Z: " << zCentreOfMass << std::endl;
+  std::cout << "---" << std::endl;
+
+  std::cout << "Moment of inertia matrix: " << std::endl;
+
+  // Pretty print the MOI matrix
+  std::stringstream ss;
+  ss << totalInertial.Moi();
+
+  std::string s;
+  long unsigned int maxLength = 0;
+  std::vector<std::string> moiVector;
+  while ( std::getline(ss, s, ' ' ) ) {
+    moiVector.push_back(s);
+    if (s.size() > maxLength) {
+      maxLength = s.size();
+    }
+  }
+
+  for (int i = 0; i < 9; i++) {
+    int spacePadding = maxLength - moiVector[i].size();
+    // Print the matrix element
+    std::cout << moiVector[i];
+    for (int j = 0; j < spacePadding; j++) {
+      std::cout << " ";
+    }
+    // Add space for the next element
+    std::cout << "  ";
+    // Add '\n' if the next row is about to start
+    if ((i+1)%3 == 0) {
+      std::cout << "\n";
+    }
+  }
+  std::cout << "---" << std::endl;
 
   return 0;
 }
