52d206549115fcdde03b3031a4d8e6ce1973a255
ahcorde <ahcorde@gmail.com>
2022-04-06 16:00:29 +0200

    Added Geometry Python interface
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- python/src/sdf/pyGeometry.hh
+++ python/src/sdf/pyGeometry.hh
@@ -14,14 +14,28 @@
  * limitations under the License.
  */
 
+#ifndef SDFORMAT_PYTHON_GEOMETRY_HH_
+#define SDFORMAT_PYTHON_GEOMETRY_HH_
+
 #include <pybind11/pybind11.h>
 
-#include "pyError.hh"
-#include "pyGeometry.hh"
+#include "sdf/Geometry.hh"
 
-PYBIND11_MODULE(sdformat, m) {
-  m.doc() = "sdformat Python Library.";
+#include "sdf/config.hh"
 
-  sdf::python::defineError(m);
-  sdf::python::defineGeometry(m);
-}
+namespace sdf
+{
+// Inline bracket to help doxygen filtering.
+inline namespace SDF_VERSION_NAMESPACE {
+namespace python
+{
+/// Define a pybind11 wrapper for an sdf::Geometry
+/**
+ * \param[in] module a pybind11 module to add the definition to
+ */
+void defineGeometry(pybind11::object module);
+}  // namespace python
+}  // namespace SDF_VERSION_NAMESPACE
+}  // namespace sdf
+
+#endif  // SDFORMAT_PYTHON_GEOMETRY_HH_
