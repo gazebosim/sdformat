6bb0965394fce565121b96af42017fdf5afc32ba
Alejandro Hernández <ahcorde@gmail.com>
2022-04-11 17:03:00 +0200

    Added Frame Python interface
    Signed-off-by: Alejandro Hernández <ahcorde@gmail.com>

--- python/src/sdf/pyFrame.hh
+++ python/src/sdf/pyFrame.hh
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
+#ifndef SDFORMAT_PYTHON_FRAME_HH_
+#define SDFORMAT_PYTHON_FRAME_HH_
+
 #include <pybind11/pybind11.h>
 
-#include "pyBox.hh"
-#include "pyError.hh"
-#include "pyFrame.hh"
-#include "pyMesh.hh"
-#include "pyParserConfig.hh"
-#include "pySphere.hh"
+#include "sdf/Frame.hh"
 
-PYBIND11_MODULE(sdformat, m) {
-  m.doc() = "sdformat Python Library.";
+#include "sdf/config.hh"
 
-  sdf::python::defineBox(m);
-  sdf::python::defineError(m);
-  sdf::python::defineFrame(m);
-  sdf::python::defineMesh(m);
-  sdf::python::defineParserConfig(m);
-  sdf::python::defineSphere(m);
-}
+namespace sdf
+{
+// Inline bracket to help doxygen filtering.
+inline namespace SDF_VERSION_NAMESPACE {
+namespace python
+{
+/// Define a pybind11 wrapper for an sdf::Frame
+/**
+ * \param[in] module a pybind11 module to add the definition to
+ */
+void defineFrame(pybind11::object module);
+}  // namespace python
+}  // namespace SDF_VERSION_NAMESPACE
+}  // namespace sdf
+
+#endif  // SDFORMAT_PYTHON_FRAME_HH_
