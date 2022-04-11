1491d7235a4073d4aa9f00f3451811107ba46297
Aditya Pande <aditya050995@gmail.com>
2022-04-11 14:30:04 -0700

    Backport ``ign sdf --inertial-stats`` (#958)
    * Ported changes from https://github.com/ignitionrobotics/sdformat/pull/936
    * Removed quick return logic
    
    Signed-off-by: Aditya <aditya050995@gmail.com>

--- src/ign.cc
+++ src/ign.cc
@@ -16,13 +16,19 @@
 */
 
 #include <iostream>
+#include <string>
 #include <string.h>
+#include <vector>
 
 #include "sdf/sdf_config.h"
 #include "sdf/Filesystem.hh"
+#include "sdf/Link.hh"
+#include "sdf/Model.hh"
 #include "sdf/Root.hh"
 #include "sdf/parser.hh"
 #include "sdf/system_util.hh"
+
+#include "ignition/math/Inertial.hh"
 
 #include "ign.hh"
 
@@ -150,6 +156,116 @@ extern "C" SDFORMAT_VISIBLE int cmdPrint(const char *_path)
   }
 
   sdf->PrintValues();
+
+  return 0;
+}
+
+//////////////////////////////////////////////////
+extern "C" SDFORMAT_VISIBLE int cmdInertialStats(
+    const char *_path)
+{
+  if (!sdf::filesystem::exists(_path))
+  {
+    std::cerr << "Error: File [" << _path << "] does not exist.\n";
+    return -1;
+  }
+
+  sdf::Root root;
+  sdf::Errors errors = root.Load(_path);
+  if (!errors.empty())
+  {
+    for (auto &error : errors)
+    {
+      std::cerr << "Error: " << error.Message() << std::endl;
+    }
+  }
+
+  if (root.WorldCount() > 0)
+  {
+    std::cerr << "Error: Expected a model file but received a world file."
+            << std::endl;
+    return -1;
+  }
+
+  const sdf::Model *model = root.ModelByIndex(0);
+  if (!model)
+  {
+    std::cerr << "Error: Could not find the model." << std::endl;
+    return -1;
+  }
+
+  if (model->ModelCount() > 0)
+  {
+    std::cout << "Warning: Inertial properties of links in nested"
+            " models will not be included." << std::endl;
+  }
+
+  ignition::math::Inertiald totalInertial;
+
+  for (uint64_t i = 0; i < model->LinkCount(); i++)
+  {
+    ignition::math::Pose3d linkPoseRelativeToModel;
+    errors = model->LinkByIndex(i)->SemanticPose().
+      Resolve(linkPoseRelativeToModel, "__model__");
+
+    auto currentLinkInertial = model->LinkByIndex(i)->Inertial();
+    currentLinkInertial.SetPose(linkPoseRelativeToModel *
+      currentLinkInertial.Pose());
+    totalInertial += currentLinkInertial;
+  }
+
+  auto totalMass = totalInertial.MassMatrix().Mass();
+  auto xCentreOfMass = totalInertial.Pose().Pos().X();
+  auto yCentreOfMass = totalInertial.Pose().Pos().Y();
+  auto zCentreOfMass = totalInertial.Pose().Pos().Z();
+
+  std::cout << "Inertial statistics for model: " << model->Name() << std::endl;
+  std::cout << "---" << std::endl;
+  std::cout << "Total mass of the model: " << totalMass << std::endl;
+  std::cout << "---" << std::endl;
+
+  std::cout << "Centre of mass in model frame: " << std::endl;
+  std::cout << "X: " << xCentreOfMass << std::endl;
+  std::cout << "Y: " << yCentreOfMass << std::endl;
+  std::cout << "Z: " << zCentreOfMass << std::endl;
+  std::cout << "---" << std::endl;
+
+  std::cout << "Moment of inertia matrix: " << std::endl;
+
+  // Pretty print the MOI matrix
+  std::stringstream ss;
+  ss << totalInertial.Moi();
+
+  std::string s;
+  size_t maxLength = 0u;
+  std::vector<std::string> moiVector;
+  while ( std::getline(ss, s, ' ' ) )
+  {
+    moiVector.push_back(s);
+    if (s.size() > maxLength)
+    {
+      maxLength = s.size();
+    }
+  }
+
+  for (int i = 0; i < 9; i++)
+  {
+    size_t spacePadding = maxLength - moiVector[i].size();
+    // Print the matrix element
+    std::cout << moiVector[i];
+    for (size_t j = 0; j < spacePadding; j++)
+    {
+      std::cout << " ";
+    }
+    // Add space for the next element
+    std::cout << "  ";
+    // Add '\n' if the next row is about to start
+    if ((i+1)%3 == 0)
+    {
+      std::cout << "\n";
+    }
+  }
+  std::cout << "---" << std::endl;
 
   return 0;
 }
