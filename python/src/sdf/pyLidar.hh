d2a73eb3f6b3775fa9ee9791be698baf157a7d0b
Alejandro Hernández <ahcorde@gmail.com>
2022-04-12 17:52:20 +0200

    Added Lidar Python interface
    Signed-off-by: Alejandro Hernández <ahcorde@gmail.com>

--- python/src/sdf/pyLidar.hh
+++ python/src/sdf/pyLidar.hh
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
+#ifndef SDFORMAT_PYTHON_LIDAR_HH_
+#define SDFORMAT_PYTHON_LIDAR_HH_
+
 #include <pybind11/pybind11.h>
 
-#include "pyBox.hh"
-#include "pyError.hh"
-#include "pyLidar.hh"
-#include "pyMesh.hh"
-#include "pyParserConfig.hh"
-#include "pySphere.hh"
+#include "sdf/Lidar.hh"
 
-PYBIND11_MODULE(sdformat, m) {
-  m.doc() = "sdformat Python Library.";
+#include "sdf/config.hh"
 
-  sdf::python::defineBox(m);
-  sdf::python::defineError(m);
-  sdf::python::defineLidar(m);
-  sdf::python::defineMesh(m);
-  sdf::python::defineParserConfig(m);
-  sdf::python::defineSphere(m);
-}
+namespace sdf
+{
+// Inline bracket to help doxygen filtering.
+inline namespace SDF_VERSION_NAMESPACE {
+namespace python
+{
+/// Define a pybind11 wrapper for an sdf::Lidar
+/**
+ * \param[in] module a pybind11 module to add the definition to
+ */
+void defineLidar(pybind11::object module);
+}  // namespace python
+}  // namespace SDF_VERSION_NAMESPACE
+}  // namespace sdf
+
+#endif  // SDFORMAT_PYTHON_LIDAR_HH_
