a2986774406f9a087294008bf9e20e0401910434
ahcorde <ahcorde@gmail.com>
2022-04-20 20:00:08 +0200

    Added Pbr and PbrWorkflow Python interfaces
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- python/src/sdf/_ignition_sdformat_pybind11.cc
+++ python/src/sdf/_ignition_sdformat_pybind11.cc
@@ -27,6 +27,7 @@
 #include "pyMesh.hh"
 #include "pyNoise.hh"
 #include "pyParserConfig.hh"
+#include "pyPbr.hh"
 #include "pyPlane.hh"
 #include "pySemanticPose.hh"
 #include "pySphere.hh"
@@ -47,6 +48,8 @@ PYBIND11_MODULE(sdformat, m) {
   sdf::python::defineMesh(m);
   sdf::python::defineNoise(m);
   sdf::python::defineParserConfig(m);
+  sdf::python::definePbr(m);
+  sdf::python::definePbrWorkflow(m);
   sdf::python::definePlane(m);
   sdf::python::defineSemanticPose(m);
   sdf::python::defineSphere(m);
