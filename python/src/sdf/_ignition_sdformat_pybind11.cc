f2e047aecce8991a563d347b19df9907d2b36556
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-04-22 09:49:08 +0200

    Added JointAxis Python interface (#960)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- python/src/sdf/_ignition_sdformat_pybind11.cc
+++ python/src/sdf/_ignition_sdformat_pybind11.cc
@@ -23,6 +23,7 @@
 #include "pyEllipsoid.hh"
 #include "pyError.hh"
 #include "pyGeometry.hh"
+#include "pyJointAxis.hh"
 #include "pyMaterial.hh"
 #include "pyMesh.hh"
 #include "pyNoise.hh"
@@ -43,6 +44,7 @@ PYBIND11_MODULE(sdformat, m) {
   sdf::python::defineEllipsoid(m);
   sdf::python::defineError(m);
   sdf::python::defineGeometry(m);
+  sdf::python::defineJointAxis(m);
   sdf::python::defineMaterial(m);
   sdf::python::defineMesh(m);
   sdf::python::defineNoise(m);
