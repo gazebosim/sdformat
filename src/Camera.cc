bdf731e9b5cf102aca91ece5cee99869561a2d44
marqrazz <marq.razz@gmail.com>
2022-08-11 14:20:26 -0600

    Add camera optical_frame_id element
    Signed-off-by: marqrazz <marq.razz@gmail.com>

--- src/Camera.cc
+++ src/Camera.cc
@@ -142,6 +142,9 @@ class sdf::Camera::Implementation
   /// \brief Frame of the pose.
   public: std::string poseRelativeTo = "";
 
+  /// \brief Frame ID the camera_info message header is expressed.
+  public: std::string opticalFrameId{""};
+
   /// \brief Lens type.
   public: std::string lensType{"stereographic"};
 
@@ -347,6 +350,13 @@ Errors Camera::Load(ElementPtr _sdf)
 
   // Load the pose. Ignore the return value since the pose is optional.
   loadPose(_sdf, this->dataPtr->pose, this->dataPtr->poseRelativeTo);
+
+  // Load the optional optical_frame_id value.
+  if (_sdf->HasElement("optical_frame_id"))
+  {
+    this->dataPtr->opticalFrameId = _sdf->Get<std::string>("optical_frame_id", 
+        this->dataPtr->opticalFrameId).first;
+  }
 
   // Load the lens values.
   if (_sdf->HasElement("lens"))
@@ -692,7 +702,8 @@ bool Camera::operator==(const Camera &_cam) const
     this->SaveFrames() == _cam.SaveFrames() &&
     this->SaveFramesPath() == _cam.SaveFramesPath() &&
     this->ImageNoise() == _cam.ImageNoise() &&
-    this->VisibilityMask() == _cam.VisibilityMask();
+    this->VisibilityMask() == _cam.VisibilityMask() &&
+    this->OpticalFrameId() == _cam.OpticalFrameId();
 }
 
 //////////////////////////////////////////////////
@@ -807,6 +818,18 @@ void Camera::SetRawPose(const ignition::math::Pose3d &_pose)
 void Camera::SetPoseRelativeTo(const std::string &_frame)
 {
   this->dataPtr->poseRelativeTo = _frame;
+}
+
+/////////////////////////////////////////////////
+const std::string Camera::OpticalFrameId() const
+{
+  return this->dataPtr->opticalFrameId;
+}
+
+/////////////////////////////////////////////////
+void Camera::SetOpticalFrameId(const std::string &_frame)
+{
+  this->dataPtr->opticalFrameId = _frame;
 }
 
 /////////////////////////////////////////////////
@@ -1148,6 +1171,8 @@ sdf::ElementPtr Camera::ToElement() const
     elem->GetElement("segmentation_type")->Set<std::string>(
         this->SegmentationType());
   }
+
+  elem->GetElement("optical_frame_id")->Set<std::string>(this->OpticalFrameId());
 
   return elem;
 }
