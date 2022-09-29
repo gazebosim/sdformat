26569a57cc2a5ca19abb79c30f7610aa8d286bfe
Steve Peters <scpeters@openrobotics.org>
2022-09-28 18:56:50 -0700

    parser_urdf: add //frame for reduced links/joints (#1148)
    Currently the location of some links and joints is lost
    when fixed joints are reduced and links lumped together.
    This adds //model/frame tags with the same name and
    pose of the reduced links and joints.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/parser_urdf.cc
+++ src/parser_urdf.cc
@@ -412,6 +412,42 @@ void ReduceFixedJoints(TiXmlElement *_root, urdf::LinkSharedPtr _link)
     sdfdbg << "Fixed Joint Reduction: extension lumping from ["
            << _link->name << "] to [" << _link->getParent()->name << "]\n";
 
+    // Add //model/frame tag to memorialize reduced joint
+    std::stringstream ssj;
+    ssj << "<frame name='" << _link->parent_joint->name << "'"
+        << " attached_to='" << _link->getParent()->name << "'>\n";
+    ssj << "  <pose>"
+        << CopyPose(_link->parent_joint->parent_to_joint_origin_transform)
+        << "</pose>\n";
+    ssj << "</frame>\n";
+
+    // Add //model/frame tag to memorialize reduced link
+    std::stringstream ssl;
+    ssl << "<frame name='" << _link->name + "'"
+        << " attached_to='" << _link->parent_joint->name << "'/>\n";
+
+    // Serialize sdf::Frame objects to xml and add to SDFExtension
+    SDFExtensionPtr sdfExt = std::make_shared<SDFExtension>();
+    auto stringToExtension = [&sdfExt](const std::string &_frame)
+    {
+      TiXmlDocument xmlNewDoc;
+      xmlNewDoc.Parse(_frame.c_str());
+      if (xmlNewDoc.Error())
+      {
+        sdferr << "Error while parsing serialized frames: "
+               << xmlNewDoc.ErrorDesc() << '\n';
+      }
+
+      TiXmlElementPtr blob =
+          std::make_shared<TiXmlElement>(*xmlNewDoc.FirstChildElement());
+      sdfExt->blobs.push_back(blob);
+    };
+    stringToExtension(ssj.str());
+    stringToExtension(ssl.str());
+
+    // Add //frame tags to model extension vector
+    g_extensions[""].push_back(sdfExt);
+
     // lump sdf extensions to parent, (give them new reference _link names)
     ReduceSDFExtensionToParent(_link);
 
