4f8f6d2b4b1d013b18be9ecfb8486cc13b1a9234
Michael Carroll <michael@openrobotics.org>
2022-10-10 10:49:23 -0500

    Improve documentation
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- include/sdf/ParserConfig.hh
+++ include/sdf/ParserConfig.hh
@@ -177,11 +177,19 @@ class SDFORMAT_VISIBLE ParserConfig
   public: bool URDFPreserveFixedJoint() const;
 
   /// \brief Set the storeResolvedURIs flag value.
-  /// \param[in] _resolveURI flag value to set
+  /// \param[in] _resolveURI True to make the parser attempt to resolve any
+  /// URIs found and store them.  False to preserve original URIs
+  /// 
+  /// The Parser will use the FindFileCallback provided to attempt to resolve
+  /// URIs in the Mesh, Material, Heightmap, and Skybox DOM objects
+  /// If the FindFileCallback provides a non-empty string, the URI will be
+  /// stored in the DOM object, and the original (unresolved) URI will be
+  /// stored in the underlying Element.
   public: void SetStoreResovledURIs(bool _resolveURI);
 
   /// \brief Get the storeResolvedURIs flag value.
-  /// \return Current flag value
+  /// \return True if the parser will attempt to resolve any URIs found and 
+  /// store them.  False to preserve original URIs
   public: bool StoreResolvedURIs() const;
 
   /// \brief Private data pointer.
