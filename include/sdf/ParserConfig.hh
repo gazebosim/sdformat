27bf7b767af842fe8382f567b444fb360151791c
Aaron Chong <aaronchongth@gmail.com>
2023-02-21 06:55:13 +0000

    Update brief of URDFSetConvertLinkWithNoMassToFrame to mention case of no inertial frame
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- include/sdf/ParserConfig.hh
+++ include/sdf/ParserConfig.hh
@@ -184,13 +184,13 @@ class SDFORMAT_VISIBLE ParserConfig
 
   /// \brief Set the convertLinkWithNoMassToFrame flag value, which determines
   /// whether to convert URDF links with small (equal to or less than
-  /// URDFMinimumAllowedLinkMass) or no mass to frames in SDF.
+  /// URDFMinimumAllowedLinkMass) or no inertial tag to frames in SDF.
   public: void URDFSetConvertLinkWithNoMassToFrame(
       bool _convertLinkWithNoMassToFrame);
 
   /// \brief Get the convertLinkWithNoMassToFrame flag value, which determines
   /// whether to convert URDF links with small (equal to or less than
-  /// URDFMinimumAllowedLinkMass) or no mass to frames in SDF.
+  /// URDFMinimumAllowedLinkMass) or no inertial tag to frames in SDF.
   public: bool URDFConvertLinkWithNoMassToFrame() const;
 
   /// \brief Private data pointer.
