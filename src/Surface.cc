849be12ec3ab12ec2e8b69cc70472dfa56fa8806
Marco A. Gutierrez <marco@openrobotics.org>
2023-03-30 11:40:44 +0800

    Add missing values in Surace ToElement method (#1263)
    Signed-off-by: Marco A. Gutierrez <marco@openrobotics.org>

--- src/Surface.cc
+++ src/Surface.cc
@@ -399,5 +399,13 @@ sdf::ElementPtr Surface::ToElement() const
   contactElem->GetElement("collide_bitmask")->Set(
       this->dataPtr->contact.CollideBitmask());
 
+  sdf::ElementPtr frictionElem = elem->GetElement("friction");
+  sdf::ElementPtr ode = frictionElem->GetElement("ode");
+  ode->GetElement("mu")->Set(this->dataPtr->friction.ODE()->Mu());
+  ode->GetElement("mu2")->Set(this->dataPtr->friction.ODE()->Mu2());
+  ode->GetElement("slip1")->Set(this->dataPtr->friction.ODE()->Slip1());
+  ode->GetElement("slip2")->Set(this->dataPtr->friction.ODE()->Slip2());
+  ode->GetElement("fdir1")->Set(this->dataPtr->friction.ODE()->Fdir1());
+
   return elem;
 }
