ceee6ff7f6ca817bfb948a37fdd873618be40e81
Ian Chen <ichen@openrobotics.org>
2024-03-20 17:41:37 -0700

    backport - Add mesh optimization attribute to <mesh>  #1382
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- test/integration/geometry_dom.cc
+++ test/integration/geometry_dom.cc
@@ -179,6 +179,12 @@ TEST(DOMGeometry, Shapes)
   EXPECT_EQ(sdf::GeometryType::MESH, meshCol->Geom()->Type());
   const sdf::Mesh *meshColGeom = meshCol->Geom()->MeshShape();
   ASSERT_NE(nullptr, meshColGeom);
+  EXPECT_EQ("convex_decomposition", meshColGeom->OptimizationStr());
+  EXPECT_EQ(sdf::MeshOptimization::CONVEX_DECOMPOSITION,
+            meshColGeom->Optimization());
+  ASSERT_NE(nullptr, meshColGeom->ConvexDecomposition());
+  EXPECT_EQ(4u, meshColGeom->ConvexDecomposition()->MaxConvexHulls());
+
   EXPECT_EQ("https://fuel.gazebosim.org/1.0/an_org/models/a_model/mesh/"
       "mesh.dae", meshColGeom->Uri());
   EXPECT_TRUE(gz::math::Vector3d(0.1, 0.2, 0.3) ==
